f67c852c84271f1f22b38655699fc4850a7e68e2
KhalilC <klcrumpler@gmail.com>
2016-01-20 03:08:02 -0500

    Done.

---
+++ models/pirate.rb
@@ -0,0 +1,12 @@
+class Pirate
+  attr_accessor :name, :height, :weight
+  
+  def initialize(args)
+    @name = args[:name]
+    @height = args[:height]
+    @weight = args[:weight]
+    
+  end
+
+
+end
