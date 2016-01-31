575ac069a062f163b1cca90da340bb710b59d363
azheng249 <andy.zheng249@gmail.com>
2016-01-30 21:06:26 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,9 +1,31 @@
 require './environment'
+require './models/pirate'
+require './models/ship'
+
+require 'pry'
 
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
+    get '/' do
+      erb :root
+    end
+
+    get '/new' do
+      erb :"pirates/new"
+    end
+
+    post '/pirates' do
+      @pirate = Pirate.new(params[:pirate])
+
+      params[:pirate][:ships].each do |ship_info|
+        Ship.new(ship_info) if not ship_info[:name].empty?
+      end
+
+      @ships = Ship.all
+
+      erb :"pirates/show"
+    end
 
   end
 end
