ea819632a162761f2c6173d941936181bd972643
benser1 <briganenser28@gmail.com>
2016-02-01 14:46:41 -0700

    Done.

---
+++ models/ship.rb
@@ -0,0 +1,20 @@
+class Ship
+
+  attr_accessor :name, :type, :booty
+
+  SHIPS = []
+
+  def initialize(name, type, booty)
+    @name = params[:name]
+    @type = params[:type]
+    @booty = params[:booty]
+    SHIPS << self
+  end
+
+  def self.all 
+    SHIPS
+  end
+
+
+
+end ## class end
