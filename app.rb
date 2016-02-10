526c432fe4f55b7c24f35faf965b0b8ef62dce43
empireofryan <rjohnson2001@gmail.com>
2016-02-10 16:13:15 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,47 @@
 require './environment'
-require_relative 'models/pirate.rb'
-require_relative 'models/ship.rb'
+# require_relative 'models/pirate.rb'
+# require_relative 'models/ship.rb'
+
+class Pirate
+
+  attr_reader :name, :weight, :height
+
+  PIRATES = []
+
+  def initialize(params)
+    @name = params[:name]
+    @weight = params[:weight]
+    @height = params[:height]
+    @ships = params[:ships]
+    PIRATES << self
+  end
+
+  def self.all 
+    PIRATES
+  end
+
+end
+
+class Ship
+
+  attr_reader :name, :type, :booty
+
+  SHIPS = []
+
+  def initialize(params)
+    @name = params[:name]
+    @type = params[:type]
+    @booty = params[:booty]
+    SHIPS << self
+  end
+
+  def self.all 
+    SHIPS
+  end
+
+end
+
+
 
 module FormsLab
   class App < Sinatra::Base
