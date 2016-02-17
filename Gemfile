7afee075b08b34ec230d2845c3b065cf70a59180
authorbeard <msnolan@gmail.com>
2016-02-16 21:49:49 -0500

    all tests passing

--- Gemfile
+++ Gemfile
@@ -5,9 +5,15 @@ gem 'sinatra-reloader', require: false
 gem 'shotgun'
 gem 'pry'
 gem 'tux'
+gem 'activerecord', :require => 'active_record'
+gem 'sqlite3'
+gem 'sinatra-activerecord', :require => 'sinatra/activerecord'
+gem 'rake'
 
 group :test do
   gem 'rspec'
   gem 'rack-test'
   gem 'capybara'
 end
+
+
--- app.rb
+++ app.rb
@@ -3,7 +3,33 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
+
+   get '/' do
+    erb :index
+   end
+
+   get '/new' do
+    erb :"pirates/new"
+
+   end
+
+   post '/pirates' do
+
+    @yar = Pirate.new(params[:pirate])
+# binding.pry
+    params[:pirate][:ships].each {|ship|
+      Ship.new(ship)
+    }
+
+    @ships = Ship.all
+
+    erb :"pirates/show"
+   end
+
+   get '/pirates' do
+    
+    erb :"pirates/show"
+   end
 
   end
 end
