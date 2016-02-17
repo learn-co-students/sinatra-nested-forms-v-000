5710972bb30651de3c40a85c8d2027b227c1a7d2
authorbeard <msnolan@gmail.com>
2016-02-16 22:05:18 -0500

    refactored post /pirates to prevent display of empty Ship objects

--- app.rb
+++ app.rb
@@ -10,24 +10,22 @@ module FormsLab
 
    get '/new' do
     erb :"pirates/new"
-
    end
 
    post '/pirates' do
 
     @yar = Pirate.new(params[:pirate])
-# binding.pry
     params[:pirate][:ships].each {|ship|
       Ship.new(ship)
     }
 
     @ships = Ship.all
+    @ships.delete_if{|ship| ship.name.empty?}
 
     erb :"pirates/show"
    end
 
-   get '/pirates' do
-    
+   get '/pirates' do  
     erb :"pirates/show"
    end
 
