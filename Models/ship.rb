189b88b86e6ee909552444f62bf463c5923849d9
Tracy <tracetrace@gmail.com>
2016-02-26 23:32:10 +0000

    Done.

---
+++ Models/ship.rb
@@ -0,0 +1,17 @@
+class Ship
+  attr_reader :name, :type, :booty
+
+ @@ships = []
+
+  def initialize(args)
+    @name = args[:name]
+    @type = args[:type]
+    @booty = args[:booty]
+    @@ships << self
+  end
+
+  def self.all
+    @@ships
+  end
+
+end
