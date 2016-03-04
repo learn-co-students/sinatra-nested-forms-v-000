ce276e8fa15e6f4f5c0aa49da4909579bc82a017
fiveinfinity <jlachance1@gmail.com>
2016-03-04 14:51:55 -0600

    all but one rspec passing

--- app.rb
+++ app.rb
@@ -1,9 +1,27 @@
 require './environment'
+require 'pry'
 
 module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get '/' do
+      erb :root
+    end
 
+    get '/new' do
+      erb :"/pirates/new"
+    end
+
+    post '/pirates' do
+      Pirate.new(params[:pirate])
+      @pirate = Pirate.all
+
+      params[:pirate][:ships].each do |key|
+        Ship.new(key)
+      end
+        @ships = Ship.all
+        erb :"/pirates/show"
+    end
   end
 end
