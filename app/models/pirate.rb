577ff1d7a353d23ffc3a1b4a5c2f23149d860822
drywaterr24 <angelolea24@gmail.com>
2016-03-06 14:18:44 -0500

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,15 @@
 class Pirate
+
+  attr_accessor :name, :weight, :height, :ships
+
+  @@pirates = []
+
+  def initialize(pirate)
+    pirate.each {|key, value| self.send("#{key}=", value)}
+    @@pirates << self
+  end
+
+  def self.all
+    @@pirates
+  end
 end
