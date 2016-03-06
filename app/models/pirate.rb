6fdd326013de0de231a364b5d82e062c527a4bfc
lcorr8 <l.corr.v@gmail.com>
2016-03-05 19:23:55 -0500

    server error

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,18 @@
 class Pirate
+
+  attr_reader :name, :weight, :height
+
+  PIRATES = []
+
+  def initialize(pirate)
+    @name = ship[:name]
+    @weight = ship[:wight]
+    @height = ship[:height]
+    PIRATES << self
+  end
+
+  def self.all?
+    PIRATES
+  end
+
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,17 @@
 class Ship
+
+  attr_reader :name, :type, :booty
+
+  SHIPS = []
+
+  def initialize(ship)
+    @name = ship[:name]
+    @type = ship[:type]
+    @booty = ship[:booty]
+    SHIPS << self
+  end
+
+  def self.all?
+    SHIPS
+  end
 end
