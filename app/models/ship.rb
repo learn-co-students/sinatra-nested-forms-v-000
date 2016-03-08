54b6e78657e805b605171a132b3da2d00650041e
ed-karabinus <ejkarabin@gmail.com>
2016-03-07 20:10:41 -0600

    Finished Sinatra Nested Forms (Pirates)

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,16 @@
 class Ship
+  attr_accessor :name, :type, :booty
+
+  @@all = Array.new
+
+  def initialize(name, type, booty)
+    self.name = name
+    self.type = type
+    self.booty = booty
+    @@all << self
+  end
+
+  def self.all 
+    @@all
+  end
 end
