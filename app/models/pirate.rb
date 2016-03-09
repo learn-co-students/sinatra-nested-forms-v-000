9d7bc282bf2f557ce23babb5c0081108503e0274
Brian Holland <brian.holland@proatria.com>
2016-03-09 16:18:54 -0600

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,14 @@
 class Pirate
+  attr_accessor :name, :weight, :height, :ships
+
+  @@pirates = []
+
+  def initialize(deets)
+    deets.each{|key,val| self.send("#{key}=", val)}
+    @@pirates.push(self)
+  end
+
+  def self.all
+    @@pirates
+  end
 end
