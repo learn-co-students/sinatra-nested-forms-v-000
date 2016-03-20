2276abccd5f8c0ba9de0d2abcc70731933e62963
Lady Codington <myemailforcoding@gmail.com>
2016-03-19 21:58:50 -0400

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,11 @@
 class Pirate
+  attr_reader :name, :weight, :height, :ships
+
+  def initialize(params)
+    @name   = params[:name]
+    @weight = params[:weight]
+    @height = params[:height]
+    @ships  = params[:ships]
+  end
+
 end
