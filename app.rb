09df0d7afd48f29b0e8af99f799c82331ac7a13f
JayThaGreat <jelson.rodriguez1012@gmail.com>
2016-03-30 18:25:25 -0400

    Done.

--- app.rb
+++ app.rb
@@ -4,6 +4,21 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get '/' do
+      erb :new
+    end
 
+    #get '/new' do
+    #  @pirate = params
+    #  erb :new
+    #end
+
+    post '/pirates' do
+      @pirate = Pirate.new(params)
+      params[:pirate][:ships].each do |key,value|
+        Ship.new(value)
+      end
+      erb :show
+    end
   end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,11 @@
 class Ship
+  attr_accessor :name, :type, :booty
+  SHIPS = []
+  def initialize(params)
+  @name = params[pirate[ships[][name]]]
+  end
+
+  def all
+    SHIPS
+  end
 end
