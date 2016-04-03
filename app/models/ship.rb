56e000f8fc15f631efd1821256be0567d37827e0
Enoch Griffith <enoch2k2@hotmail.com>
2016-04-03 16:17:53 +0000

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,17 @@
 class Ship
+  attr_accessor :name, :type, :booty
+
+  @@ships = []
+
+  def self.all
+    @@ships
+  end
+
+  def initialize(details)
+    self.name = details[:name]
+    self.type = details[:type]
+    self.booty = details[:booty]
+    @@ships << self
+  end
+
 end
