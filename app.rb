32a2ddd4fe8ff8397652197f81790cfa4d267be0
brydela <akestrelgrey@gmail.com>
2016-04-04 20:27:41 -0700

    Done.

--- app.rb
+++ app.rb
@@ -14,7 +14,11 @@ module FormsLab
 
     post '/pirates' do
       @pirates = Pirate.new(params[:pirate])
+      params[:pirate][:ship].each do |x|
+        Ship.new(x)
+      end
       @ships = Ship.all
+      binding.pry
       erb :'pirates/show'
     end
   end
