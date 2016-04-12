c4116ac74f69309fc7950e5e7aee00602f43a18f
gnfisher <me@gnfisher.com>
2016-04-12 12:51:43 -0300

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,14 @@
 class Ship
+  attr_accessor :name, :type, :booty
+  
+  @@all = []
+  
+  def initialize(args = {})
+    args.each {|k,v| send "#{k}=", v if respond_to? "#{k}="}
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 end
