c6f48fa42495b3beb50286ca8bc7c41e4face8da
Nathaniel Miller <dukeoflaser@gmail.com>
2016-04-17 05:28:55 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,9 +1,21 @@
 require './environment'
 
 module FormsLab
+  
   class App < Sinatra::Base
+    get '/' do
+      erb :'pirates/index'
+    end
 
-    # code other routes/actions here
+    get '/new' do
+      erb :'pirates/new'
+    end
+    
+    post '/pirates' do
+      @pirate_data = params
 
+      erb :'pirates/show'
+    end
   end
+  
 end
---
+++ models/pirate.rb
@@ -0,0 +1,10 @@
+class Pirate
+  attr_accessor :name, :weight, :height
+  
+  def initialize(name, weight, height)
+    @name = name
+    @weight = weight
+    @height = height
+  end
+  
+end
