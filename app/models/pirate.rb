5f149b3029f839dad46b0c54e48f73d69c005527
Amy Back <aback1227@gmail.com>
2016-04-29 23:03:26 -0400

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,12 @@
 class Pirate
+  attr_accessor :name, :weight, :height
+
+  PIRATES = []
+
+  def initialize(args)
+    @name = args[:name]
+    @weight = args[:weight]
+    @height = args[:height]
+    PIRATES << self
+  end
 end
