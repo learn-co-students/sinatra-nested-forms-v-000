a390bbb5fc60daf9f568835801c4b8ccd60d51e3
eliyooy <ebshimon@gmail.com>
2016-05-15 13:51:10 -0400

    initial commit, with 1 testing error

--- app.rb
+++ app.rb
@@ -3,7 +3,29 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
+    get '/' do
+      erb :root
+    end
+
+    get '/new' do
+      erb :'pirates/new'
+    end
+
+    post '/pirates' do
+      @pirate = Pirate.new(params[:pirate])
+
+      ship_info = @pirate.each do |ship, information|
+        information
+      end
+
+      ship_info.each do |ship|
+        Ship.new(ship)
+      end
+
+      @ships = Ship.all
+
+      erb :'pirates/show'
+    end
 
   end
 end
