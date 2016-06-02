adb2e06fda3a9ed84848ae5616a89a2c6779e12f
mcclains2003 <mcclains2003@gmail.com>
2016-06-02 12:12:00 -0500

    Push for review

--- app.rb
+++ app.rb
@@ -3,7 +3,18 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
+    get '/' do 
+      erb :root
+    end
+
+    get '/new' do 
+      erb 'pirates/new'.to_sym
+    end
+
+    post '/pirates' do
+      puts params
+      erb 'pirates/show'.to_sym
+    end
 
   end
 end
--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,18 @@
 class Pirate
+  attr_reader :name, :weight, :height
+
+  @@all_pirates = []
+
+  def intialize(params)
+    @name = params[:name]
+    @weight = params[:weight]
+    @height = params[:height]
+
+    @@all_pirates << self
+  end
+
+  def self.all
+    @@all_pirates
+  end
+
 end
