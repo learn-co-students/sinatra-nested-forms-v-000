d8aa19d3eba66547089a1780c7aa1803d4f90665
wook kim <wooke2e@gmail.com>
2016-06-22 23:13:37 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,18 @@
 class Pirate
+
+  attr_accessor :name, :weight, :height
+
+  PIRATES = []
+
+  def initialize(args)
+    @name = args[:name]
+    @weight = args[:grade]
+    @height = args[:height]
+    PIRATES << self
+  end
+
+  def self.all
+    PIRATES
+  end
+
 end
