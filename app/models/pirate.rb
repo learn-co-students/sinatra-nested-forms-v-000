38bdfe60a09b56557b83f5e4a0dbe4408b51a7f1
Joseph Lim <joseph.d.lim@gmail.com>
2016-07-01 01:20:47 +0000

    interim

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,18 @@
 class Pirate
+  attr_accessor :name, :height, :weight
+
+  PIRATES = []
+
+  def initialize(args)
+    @name = args[:name]
+    @height = args[:height]
+    @weight = args[:weight]
+    PIRATES << self
+  end
+
+  def self.all
+    PIRATES
+  end
+
+
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,24 @@
 class Ship
+  attr_accessor :name, :type, :booty
+
+  SHIPS = []
+
+  def initialize(args)
+    puts args
+    puts args["name"]
+    @name = args["name"]
+    @booty = args["booty"]
+    @type = args["type"]
+    SHIPS << self
+  end
+
+  def self.all
+    SHIPS
+  end
+
+  def self.clear
+    SHIPS.each {|ship| ship = nil}
+    SHIPS.clear
+  end
+
 end
