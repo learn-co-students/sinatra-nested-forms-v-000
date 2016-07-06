f30830bbf7fdb0b2a3ad9bc4389ea069d9398ca0
tddycks <tadzio.smith@gmail.com>
2016-07-06 17:36:24 -0400

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,23 @@
 class Pirate
+  attr_accessor :name, :weight, :height
+
+  @@all = []
+
+  def initialize(pirate)
+    @name = pirate[:name]
+    @weight = pirate[:weight]
+    @height = pirate[:height]
+    #@name, @weight, @height = name, weight, height
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear
+    @@all.clear
+  end
+
+
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,21 @@
 class Ship
+  attr_accessor :name, :type, :booty
+
+  @@all = []
+
+  def initialize(ship)
+    ship.each do |k, v|
+      self.send("#{k}=", v)
+    end
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear
+    @@all.clear
+  end
+
 end
