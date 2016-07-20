2d1f049d6cb47cd9af3388ac6f0685a25213cabc
Jason Casares <jasonc@csdrafting.com>
2016-07-20 18:16:22 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,9 +1,15 @@
 class Pirate
   attr_accessor :name, :weight, :height
+  PIRATES = []
 
-  def initialize (name, weight, height)
-    @name = name
-    @weight = weight
-    @height = height
+  def initialize (args)
+    @name = args[:name]
+    @weight = args[:weight]
+    @height = args[:height]
+    PIRATES << self
+  end
+
+  def self.all
+    PIRATES
   end
 end
