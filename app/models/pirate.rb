9595b081060ec7378cf808f55c204adc203c1582
Adam Taitano <adam.taitano@gmail.com>
2016-07-27 21:07:00 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,21 @@
 class Pirate
+
+  attr_accessor :name, :weight, :height
+
+  @@pirates = []
+
+  def initialize(name, weight, height)
+    @name = name
+    @weight = weight
+    @height = height
+    @@pirates << self
+  end
+
+  def self.all
+    @@pirates
+  end
+
+  def self.clear
+    @@pirates.clear
+  end
 end
