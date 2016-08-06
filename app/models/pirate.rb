a43b1fcfd15714bfa3f8b3911763cf485be5677c
Jennifer Pazos <jmp521@gmail.com>
2016-08-06 07:57:16 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,16 @@
+require 'pry'
 class Pirate
+  attr_accessor :name, :weight, :height
+  PIRATES = []
+
+  def initialize(input)
+    @name = input[:name]
+    @weight = input[:weight]
+    @height = input[:height]
+    PIRATES << self
+  end
+
+  def self.all
+    PIRATES
+  end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,18 @@
 class Ship
+  attr_accessor :name, :type, :booty
+  @@all = []
+  def initialize(input)
+    @name = input[:name]
+    @type= input[:type]
+    @booty = input[:booty]
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear
+    @@all = []
+  end
 end
