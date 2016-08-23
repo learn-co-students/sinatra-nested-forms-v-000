53b734910f868367cceacf5d1ed34b2e5bdfac92
ThisIsTyDell <milleb94@gmail.com>
2016-08-23 18:14:04 -0500

    add ships array to collect newly created ships

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,9 +1,15 @@
 class Ship
   attr_accessor :name, :type, :booty
 
+  SHIPS = []
+
   def initialize(name, type, booty)
     @name = name
     @type = type
     @booty = booty
+    SHIPS << self
   end
+
+  
+
 end
