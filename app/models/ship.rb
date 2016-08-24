de0b671c1d9482657642871d03c8a70013e0a7a2
Lenn Hypolite <lenntriniman@hotmail.com>
2016-08-24 21:02:28 +0000

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,20 @@
 class Ship
+  attr_accessor :name, :weight, :height
+    SHIPS = []
+
+  def initialize(arg)
+    @name = arg[:name]
+    @weight = arg[:type]
+    @height = arg[:booty]
+    SHIPS << self
+  end
+
+  def self.all
+    SHIPS
+  end
+
+  def self.clear
+    SHIPS.clear
+  end
+
 end
