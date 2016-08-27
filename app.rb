720270fbeb5a84611705efe1cd4161d523a81d88
bmolina-nyc <bruce.d.molina@gmail.com>
2016-08-27 10:18:02 -0400

    Done.

--- app.rb
+++ app.rb
@@ -5,5 +5,25 @@ module FormsLab
 
     # code other routes/actions here
 
+    get '/' do 
+      "hello world"
+      erb :index
+    end
+
+    get '/new' do
+      erb :new
+    end
+
+    post '/pirates' do 
+      
+      @pirate = Pirate.new(params[:pirate])
+      params[:pirate][:ships].each do |ship|
+        Ship.new(ship)
+      end
+
+      @ships = Ship.all    
+      erb :show
+    end
+
   end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,18 @@
 class Ship
+  attr_accessor :name, :type, :booty
+  SHIPS = [] 
+
+  def initialize (params)
+    @name, @type, @booty = params["name"], params["type"], params["booty"]
+    SHIPS << self 
+  end
+
+  def self.all 
+    SHIPS  
+  end
+
+  def self.clear
+    SHIPS.clear
+  end
+
 end
