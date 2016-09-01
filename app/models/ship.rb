89d1e5738d7a480705fead89a3d5600dc2ef2777
William Jones <enquiries@forestfed.com>
2016-09-01 20:36:05 +0000

    use nested forms

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,24 @@
 class Ship
+  attr_accessor :name, :type, :booty
+  @@ships = []
+
+  def initialize(ship)
+    @name = ship[:name]
+    @type = ship[:type]
+    @booty = ship[:booty]
+    save
+  end
+
+  def self.all
+    @@ships
+  end
+
+  def save
+    @@ships << self
+  end
+
+  def self.clear
+    @@ships = []
+  end
+
 end
