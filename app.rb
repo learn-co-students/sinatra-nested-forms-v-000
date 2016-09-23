dafb6a474a7c66be8ea92166c2901cb6b67c484b
BriOD <briodonoghue@gmail.com>
2016-09-23 13:59:00 -0400

    Done.

--- app.rb
+++ app.rb
@@ -3,12 +3,16 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
+    get '/' do
+      erb :root
+    end
+
     get '/new' do
       erb :'pirates/new'
     end
 
     post '/pirates' do
-      binding.pry
+      # binding.pry
       @pirate = Pirate.new(params[:pirate])
 
 
