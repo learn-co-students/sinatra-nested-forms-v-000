9478694910b21ec1837e56a7bc218fc34787ac03
yeungn <nin.yeung@gmail.com>
2016-10-12 10:30:34 -0400

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,18 @@
 class Ship
+  @@all = []
+
+  def initialize(params)
+    @name = params[:name]
+    @type = params[:type]
+    @booty = params[:booty]
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear
+    @@all.clear
+  end
 end
