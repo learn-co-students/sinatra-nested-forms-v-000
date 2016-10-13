81b5d4cfe252a0c94554791c6035885d84b35e3c
Clare Horton <clare.e.horton@gmail.com>
2016-10-13 00:32:32 -0500

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,17 @@
 class Pirate
+  attr_accessor :name, :weight, :height
+
+  PIRATES = []
+
+  def self.all
+    PIRATES
+  end
+
+  def initialize(name, weight, height)
+    @name = name
+    @weight = weight
+    @height = height
+    PIRATES << self
+  end
+
 end
