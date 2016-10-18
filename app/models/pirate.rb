4746839a241810b60f45eb25b0f48b0f2234bc75
Will Lynch <will.lynch91@gmail.com>
2016-10-18 19:42:30 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,16 @@
 class Pirate
+    attr_accessor :name, :weight, :height
+
+    PIRATES = []
+
+    def initialize(attr)
+        @name = attr[:name]
+        @weight = attr[:weight]
+        @height = attr[:height]
+        PIRATES << self
+    end
+
+    def self.all
+        PIRATES
+    end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,20 @@
 class Ship
+    attr_accessor :name, :type, :booty
+
+    SHIPS = []
+
+    def initialize(attr)
+        @name = attr[:name]
+        @type = attr[:type]
+        @booty = attr[:booty]
+        SHIPS << self
+    end
+
+    def self.all
+        SHIPS
+    end
+
+    def self.clear
+        SHIPS.clear
+    end
 end
