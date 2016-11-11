7c762821a25ce083f38b919f3efcbb96c87897e3
Elyse Klova <elyse.klova@gmail.com>
2016-11-11 20:15:20 +0000

    Done.

--- app.rb
+++ app.rb
@@ -13,7 +13,11 @@ module FormsLab
 
     post '/pirates' do
       @pirate = Pirate.new(params[:pirate])
-      erb :"pirates/pirates"
+      params[:pirate][:ships].each do |ship|
+        Ship.new(ship)
+      end
+      @ships = Ship.all
+      erb :"pirates/show"
     end
 
   end
