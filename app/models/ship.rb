5999eae17c309ffa7bd532ffa6de77d1120f1a0f
William Story <wstory@villanova.edu>
2016-11-18 23:41:17 +0000

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,20 @@
 class Ship
+  attr_accessor :name, :type, :booty
+  SHIPS = []
+
+  def initialize(hash)
+    hash.each do |key, value|
+      self.send("#{key}=", value)
+    end
+    SHIPS << self
+  end
+
+  def self.all
+    SHIPS
+  end
+
+  def self.clear
+    SHIPS.clear
+  end
+
 end
