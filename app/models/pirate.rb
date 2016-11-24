f84761eedaeb2195da4462f435db8ab665309040
Ray Mulligan <rmullig2@optonline.net>
2016-11-24 00:50:03 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,13 @@
 class Pirate
+
+  attr_accessor :name, :weight, :height
+  @@pirates = []
+
+  def initialize(name, weight, height)
+    @name = name
+    @weight = weight
+    @height = height
+    @@pirates << self
+  end
+
 end
