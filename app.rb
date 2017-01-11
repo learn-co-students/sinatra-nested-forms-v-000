1bccddab9aed8b82f4bacb8de97e33bc09c4bad6
Mary Umoh <umohm12@gmail.com>
2017-01-11 22:03:39 +0000

    Done.

--- app.rb
+++ app.rb
@@ -13,15 +13,9 @@ module FormsLab
     end
 
   post '/pirates' do
-    #binding.pry
-    #@pirate = [params[:name], params[:weight], params[:height]]
     @pirate = Pirate.new(name: params[:pirate][:name], weight: params[:pirate][:weight], height: params[:pirate][:height])
     @ship_1 = Ship.new(name: params[:pirate][:ships][0][:name], type: params[:pirate][:ships][0][:type], booty: params[:pirate][:ships][0][:booty])
     @ship_2 = Ship.new(name: params[:pirate][:ships][1][:name], type: params[:pirate][:ships][1][:type], booty: params[:pirate][:ships][1][:booty])
-
-    #@ship = params[:name], params[:type], params[:booty
-    #@pirates = Pirates.all
-    #@pirates = Pirate.new(params[:name], params[:weight], params[:height])
     erb :'pirates/show'
   end
  end
