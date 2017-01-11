147f38b6f3046fe26f974d730e47ec553d425ab0
mo6709 <mo6709@gmail.com>
2017-01-11 11:40:23 -0500

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,22 @@
 class Pirate
+	attr_accessor :name, :weight, :height
+  
+  PIRATES = []
+
+	def initialize(hash)
+		@name = hash["name"]
+		@weight = hash["weight"]
+		@height = hash["height"]
+
+		PIRATES << self
+	end
+  
+  def self.all
+  	PIRATES
+  end
+
+  def self.clear
+  	PIRATES.clear
+  end
+
 end
