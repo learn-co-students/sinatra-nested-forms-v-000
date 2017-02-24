6d8b1e009128c31b94da25eed50e774111c90a2c
Kevin Adkins <kevinladkins@gmail.com>
2017-02-24 16:44:30 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,9 @@
 class Pirate
+  attr_accessor :name, :weight, :height
+
+  def initialize(attributes_hash)
+    attributes_hash.each {|k, v|
+      self.send(("#{k}="), v)}
+  end
+
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,19 @@
 class Ship
+  @@all = []
+
+  attr_accessor :name, :type, :booty
+
+  def initialize(attributes)
+    attributes.each {|k, v|
+      self.send(("#{k}="), v)}
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear
+    @@all.clear
+  end
+
 end
