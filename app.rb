33cc67b062f22528bb72902ae4f8ccf25ad03e3b
Michael O'Connell <michaeldavidoconnell@gmail.com>
2017-03-11 15:55:38 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,9 +1,29 @@
 require './environment'
-
+#require_relative './views/pirates/new.erb'
+#require_relative './views/pirates/show.erb'
 module FormsLab
-  class App < Sinatra::Base
-
-    # code other routes/actions here
-
-  end
+    class App < Sinatra::Base
+        get '/' do
+            "Welcome to the Nested Forms Lab!\n let's navigate to the '/new'" 
+        end
+        
+        get '/new' do
+            erb :'pirates/new'
+        end
+        
+        
+        post '/pirates' do
+            @pirate = Pirate.new(params[:pirate])
+            
+            params[:pirate][:ships].each do |ship|
+                Ship.new(ship) 
+            end
+#Ship.new(name: params[:ship_name_1],type: params[:ship_type_1],booty: params[:ship_booty_1])
+#Ship.new(name: params[:ship_name_2],type: params[:ship_type_2],booty: params[:ship_booty_2])            
+            
+            @ships = Ship.all
+            
+            erb :'pirates/show'
+        end
+    end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,26 @@
 class Ship
+    attr_accessor :name, :type, :booty
+
+    @@all =[]
+
+    def initialize(stats = {})
+        stats.each do |key,value|
+            send("#{key}=",value) 
+        end
+
+        save
+    end
+
+    def self.all
+        @@all
+    end
+    
+    def save
+        self.class.all << self
+    end
+    
+    def self.clear
+        self.all.clear
+    end
+
 end
