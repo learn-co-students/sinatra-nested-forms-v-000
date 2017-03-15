68e94d6989ffdbced1d110c00aa9baf72c57a071
bentonwong <bentonwong@gmail.com>
2017-03-15 10:28:40 -0700

    add welcome message to '/' page

--- app.rb
+++ app.rb
@@ -5,6 +5,11 @@ module FormsLab
 
     # code other routes/actions here
     get '/' do
+      "Welcome to the Nested Forms Lab!"
+      "let's navigate to the '/new'"
+    end
+
+    get '/new' do
       erb :new
     end
 
