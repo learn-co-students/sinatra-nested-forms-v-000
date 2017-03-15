492d3fcb8b66df262344b8b073915b6acdce69da
bentonwong <bentonwong@gmail.com>
2017-03-15 09:52:19 -0700

    add ship.rb and pirate.rb

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,15 @@
 class Pirate
+
+  attr_accessor :name, :weight, :height
+
+  def initialize
+      @name = name
+      @weight = weight
+      @height = height
+  end
+
+  def self.all
+    @@all
+  end
+
 end
