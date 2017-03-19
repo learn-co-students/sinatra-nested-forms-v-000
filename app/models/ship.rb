da17eef317aa9e896d9407c5dc8ad44cf50fd169
btcline04 <btcline04@gmail.com>
2017-03-18 21:19:57 -0400

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,22 @@
 class Ship
+
+  attr_reader :name, :type, :booty
+
+  SHIP = []
+
+  def initialize(params)
+    @name = params[:name]
+    @type = params[:type]
+    @booty = params[:booty]
+    SHIP << self
+  end
+
+  def self.all
+    SHIP.all
+  end
+
+  def self.clear
+    SHIP.clear
+  end
+
 end
