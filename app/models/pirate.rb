710bcf98c7d986242c9e13923c58cd9106764def
Kenneth Holmquist <madifana@yahoo.com>
2017-03-21 01:44:14 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,12 @@
 class Pirate
+  attr_accessor :name, :height, :weight
+
+  @@all = []
+
+  def initialize(params)
+    @name = params[:name]
+    @weight = params[:weight]
+    @height = params[:height]
+    @@all << self
+  end
 end
