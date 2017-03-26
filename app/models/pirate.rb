c805e59d1672d399f45f1522e541c85659ab6fe0
Levio Marotti III <lm3@protonmail.com>
2017-03-25 22:03:04 -0500

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,21 @@
 class Pirate
+  attr_reader :name, :weight, :height
+
+  @@pirates = []
+
+  def initialize
+    @name = name
+    @weight = weight
+    @height = height
+    @@pirates << self
+  end
+
+  def self.all
+    @@pirates
+  end
+
+  def self.clear
+    @@pirates = []
+  end
+
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,16 @@
 class Ship
+  attr_reader :name, :type, :booty
+
+  @@ships = []
+
+  def initiailze
+    @name = name
+    @type = type
+    @booty = booty
+    @@ships << self
+  end
+
+  def self.all
+    @@ships
+  end
 end
