ab4e418a9a3ff54227e1014f06ad368d50b77d76
Efrain Perez Jr <efrainperezjr@live.com>
2017-03-28 19:09:07 +0000

    Done.

--- app.rb
+++ app.rb
@@ -4,6 +4,20 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get "/" do
+      erb :index
+    end
+
+    get "/new" do
+      erb :new, :views => 'views/pirates'
+    end
+
+    post "/pirates" do
+      @pirate = Pirate.new(params[:pirate])
+      Ship.create_from_collection(params[:pirate][:ships])
+      @ships = Ship.all
+      erb :show, :views => 'views/pirates'
+    end
 
   end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,25 @@
 class Ship
+
+  attr_accessor :name, :type, :booty
+
+  ALL = []
+  
+  def initialize(params)
+    @name = params[:name]
+    @type = params[:type]
+    @booty = params[:booty]
+    ALL << self
+  end
+
+  def self.create_from_collection(ship_info_array)
+    ship_info_array.each {|ship_info| Ship.new(ship_info)}
+  end
+
+  def self.all
+    ALL    
+  end
+
+  def self.clear
+    ALL.clear
+  end
 end
