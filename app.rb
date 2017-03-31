c88677f86f9a1cc420b89aab04cb0363059fe34d
Daniel Lempesis <danlrcn@hotmail.com>
2017-03-31 21:56:54 +0000

    Done.

--- app.rb
+++ app.rb
@@ -3,7 +3,25 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
+    get '/' do #get root path, send get request to root file
+      erb :root #sends you to :root
+    end
+
+    get '/new' do #get /new URL request, read from :pirates/new
+      erb :'pirates/new' #pirates folder/new.erb
+    end
+
+    post '/pirates' do #post /pirates url from new.erb user form submission (action = /pirates)
+      @pirate = Pirate.new(params[:pirate])
+
+      params[:pirate][:ships].each do |ship_info| #[:ships] not [:ship]. name="pirate[ships][][name] creates the hash. has nothing to do with the ruby obj yet.
+        Ship.new(ship_info)
+      end
+
+      @ships = Ship.all #sets @ships equal to all the ships created and placed in the SHIPS constant in ship.rb
+
+      erb :'pirates/show' #display from show.erb on /pirates
+    end
 
   end
 end
--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,18 @@
 class Pirate
+  attr_accessor :name, :weight, :height
+
+  PIRATES = [] #Unsure why this is a constant instead of a class method. No explanation was given. @@all *does* work though.
+
+  def initialize(params) #arg passed in as a has (e.g. pirate => {name => "Jack", weight => '175', height => "5'9"})
+    @name = params[:name] #taken from user's data upon submission
+    @weight = params[:weight]
+    @height = params[:height]
+
+    PIRATES << self
+  end
+
+  def self.all
+    PIRATES
+  end
+
 end
