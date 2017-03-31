da3329a44b8cc7c8bd47d0c40654282e94e310d4
walwoodr <walwoodr@gmail.com>
2017-03-31 14:33:10 -0500

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,10 @@
 class Pirate
+  attr_accessor :name, :height, :weight
+
+  def initialize(attr_array)
+    self.name = attr_array[:name]
+    self.height = attr_array[:height]
+    self.weight = attr_array[:weight]
+  end
+
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,22 @@
 class Ship
+  attr_accessor :name, :type, :booty
+  @@all = []
+
+  def initialize(attr_array)
+
+    attr_array.each do |key, value|
+      self.send("#{key}=", value)
+    end
+
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear
+    @@all = []
+  end
+
 end
