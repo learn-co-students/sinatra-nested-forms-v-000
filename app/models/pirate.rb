ee649b18324e0bb753960637075afba22379b8e2
bortch89 <bortch@gmail.com>
2017-04-03 12:18:13 +0200

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,18 @@
 class Pirate
+
+@@all = []
+
+  attr_accessor :name, :weight, :height
+
+  def initialize(hash)
+    @name = hash[:name]
+    @weight = hash[:weight]
+    @height = hash[:height]
+  end
+
+  def self.all
+    @@all
+  end
+
+
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,20 @@
 class Ship
+  @@all = []
+  attr_accessor :name, :type, :booty
+
+  def initialize(details)
+    @name = details[:name]
+    @type = details[:type]
+    @booty = details[:booty]
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear
+    @@all
+  end
+
 end
