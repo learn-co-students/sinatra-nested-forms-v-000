4c8185e552069cd57d81c6f3f325c68878050336
github username <github email address>
2017-04-07 18:23:37 -0400

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,20 @@
 class Pirate
+
+  attr_accessor :name, :weight, :height, :ships
+
+  @@all = []
+
+  def initialize(pirate)
+    @name = pirate['name']
+    @weight = pirate['weight']
+    @height = pirate['height']
+    @ships = pirate['ships'].map{ |ship| Ship.new(ship) }
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+
 end
