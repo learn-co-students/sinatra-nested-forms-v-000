651792be22966669fef2cd4fc5e7d01b8364f601
Aniqa Elahi <aniqaelahi@yahoo.com>
2017-04-17 18:33:30 +0000

    Done.

---
+++ Rakefile
@@ -0,0 +1,8 @@
+ENV["SINATRA_ENV"] ||= "development"
+require_relative './environment'
+require 'sinatra/rake'
+# Type `rake -T` on your command line to see the available rake tasks.
+
+task :console do
+  Pry.start
+end
--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,15 @@
+require 'pry'
 class Pirate
+  attr_accessor :name, :weight, :height
+    PIRATES = []
+  def initialize(name,weight,height)
+    @name,@weight,@height = name,weight,height
+    PIRATES << self
+  end
+
+  def self.all 
+    PIRATES
+  end
+    
+
 end
