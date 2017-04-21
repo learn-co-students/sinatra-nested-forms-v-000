68e3e6d93d69c895f7f3d9f4090bf5b43144f5ce
Erika Hughes <ehughes@knights.ucf.edu>
2017-04-21 23:16:35 +0000

    Done.

--- app.rb
+++ app.rb
@@ -5,5 +5,22 @@ module FormsLab
 
     # code other routes/actions here
 
+    get '/' do
+      erb :index
+    end
+
+    get '/new' do
+      erb :new
+    end
+
+    post '/pirates' do
+      @pirate = Pirate.new(params[:pirate])
+      params[:pirate][:ships].each do |details|
+        Ship.new(details)
+      end
+      @pirateships = Ship.all
+      erb :show
+    end
+
   end
 end
