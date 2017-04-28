d8ea219d5673c59166359b70116f3f5cdbe964b6
Andrew Bonner <andrewbonner92@gmail.com>
2017-04-28 08:50:23 -0400

    initial

--- environment.rb
+++ environment.rb
@@ -3,11 +3,6 @@ ENV['SINATRA_ENV'] ||= "development"
 require 'bundler'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
-ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3"
-  :database => "db/#{ENV['SINATRA_ENV']}.sqlite3"
-)
-
 require 'sinatra/base'
 require 'sinatra/reloader'
 
