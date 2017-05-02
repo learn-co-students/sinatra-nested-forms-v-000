3363cbd6673c529625c307455f45210d74fff890
Zenia Villa <zeniavilla@hotmail.com>
2017-05-02 12:12:06 -0700

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,22 @@
 class Pirate
+
+    attr_accessor :name, :weight, :height
+
+    PIRATES = []
+
+    def initialize(properties)
+        @name = properties[:name]
+        @weight = properties[:weight]
+        @height = properties[:height]
+        PIRATES << self
+    end
+
+    def self.all
+        PIRATES
+    end
+
+    def self.clear
+        self.all.clear
+    end
+    
 end
