38c141881d965354ee1b2f72416ff2043edf5b6c
Niki Lee <ew1415048nk@gmail.com>
2017-05-28 22:21:01 +0900

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,15 @@
 class Pirate
+  attr_accessor :name, :weight, :height
+  @@all=[]
+  def initialize(hash) #input값이 hash잖아.
+    @name = hash[:name]
+    @weight = hash[:weight]
+    @height = hash[:height]
+    self.class.all<<self
+  end
+
+  def self.all
+    @@all
+  end
+
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,20 @@
 class Ship
+  attr_accessor :name, :type, :booty
+
+  @@all=[]
+
+  def initialize(hash)
+    @name = hash[:name]
+    @type = hash[:type]
+    @booty = hash[:booty]
+    self.class.all<<self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear
+    self.all.clear
+  end
 end
