d8dd301305d26caa38860b99b8058be25b297063
Menachem Hornbacher <me@mendel.tech>
2017-05-29 15:00:10 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,7 @@
+require 'pry'
 class Pirate
+  attr_accessor :name, :weight, :height
+  def initialize(args)
+    args.each{|k, v| self.send("#{k}=", v) if self.methods.include?("#{k}=".to_sym)}
+  end
 end
