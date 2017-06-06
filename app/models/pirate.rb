e90682d7f635a912b1cad7879ec6a7bebc778c89
streamedLine <dovidlwtn@gmail.com>
2017-06-05 23:06:19 -0400

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,22 @@
 class Pirate
+	attr_accessor :name, :weight, :height
+	@@all = []
+
+	def initialize(name, weight, height)
+		@name = name
+		@weight = weight
+		@height = height
+		@@all << self
+		self
+	end
+
+	def self.clear
+		@@all = []
+	end
+
+	def self.create_from_params(params)
+		# binding.pry
+		self.new(params["name"], params["weight"], params["height"])
+	end
+
 end
