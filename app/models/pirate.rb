00230ec9de6b43173f4cbf39ce62a395ce371ff6
Joseph Kim <josephkim0224@gmail.com>
2017-06-07 19:35:50 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,17 @@
 class Pirate
+
+  attr_accessor :name, :weight, :height
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def initialize(arguments)
+    @name = arguments[:name]
+    @weight = arguments[:weight]
+    @height = arguments[:height]
+    @@all << self
+  end
 end
