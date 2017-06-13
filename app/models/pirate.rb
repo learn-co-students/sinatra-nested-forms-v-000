caa47e6c226b28e2a6fb45a5fba35dfb309a6467
Mikyle Crawford <crawfordmikyle@gmail.com>
2017-06-13 16:20:27 -0600

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,19 @@
 class Pirate
+	attr_accessor :name, :height, :weight, :ships
+	@@all =[]
+	def initialize(args)
+		@name = args[:name]
+		@height = args[:height]
+		@weight = args[:weight]
+		@ships = args[:ships]
+		@@all << self
+	end
+	
+	def self.all
+		@@all
+	end 
+	
+	def self.clear
+		@@all = []
+	end 
 end
