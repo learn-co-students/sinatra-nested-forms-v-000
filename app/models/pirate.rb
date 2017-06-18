ec0c6ebb168fe9cc678f6d55e9653d5cec8dfb28
Amanda George <github email address>
2017-06-17 22:41:14 -0500

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,22 @@
 class Pirate
+
+  attr_reader :name, :weight, :height
+
+  @@all = []
+
+  def initialize(pirate)
+    @name = pirate[:name]
+    @weight = pirate[:weight]
+    @height = pirate[:height]
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear
+    @@all.destroy
+  end
+
 end
