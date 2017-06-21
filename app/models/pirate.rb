05131bfc4eee98aaa4100bc60a7cf5e4959b6bff
Mollie Stein <mollie.stein@gmail.com>
2017-06-21 23:41:14 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,21 @@
 class Pirate
+
+  attr_accessor :name, :weight, :height
+  @@all = []
+
+  def initialize(args)
+    @name = args[:name]
+    @height = args[:height]
+    @weight = args[:weight]
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear
+    @@all.clear
+  end
+
 end
