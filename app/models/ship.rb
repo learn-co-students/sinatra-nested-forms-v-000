6ffeebfd57672b87cf0857d6a54c459b8457a09f
Andrew <github email address>
2017-06-28 11:28:18 -0400

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,21 @@
 class Ship
+
+    @@all = []
+
+    attr_accessor :name, :type, :booty
+    def initialize(details)
+        @name = details[:name]
+        @type = details[:type]
+        @booty = details[:booty]
+        @@all << self
+
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear
+        @@all.clear
+    end
 end
