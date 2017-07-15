1ea95257f7fda3ace4b80eed57d4088b38dd6c29
charlie lee <ctlee9096@gmail.com>
2017-07-15 02:49:45 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,7 +2,22 @@ require './environment'
 
 module FormsLab
   class App < Sinatra::Base
+    get '/' do
+      "Welcome to the Nested Forms Lab! let's navigate to the '/new'"
+    end
 
+    get '/new' do
+      erb :'pirates/new'
+    end
+
+    post '/pirates'do
+    @pirate = Pirate.new(params[:pirate])
+    params[:pirate][:ships].each do |ship|
+      Ship.new(ship)
+    end
+    @ships = Ship.all
+    erb :'pirates/show'
+    end
     # code other routes/actions here
 
   end
