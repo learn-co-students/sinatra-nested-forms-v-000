6b2c4b2149ac5eae1207a84b2055ff9ec0868b33
Leonardo Guzman <lguzman@me.com>
2017-07-31 05:23:05 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,18 @@
 class Pirate
+	attr_accessor :name, :weight, :height
+
+	PIRATES = []
+ #Since we are using nested forms, we need to initialize with (args)
+ # then use the syntax :'@name = args[:name]'
+	def initialize(args)
+		@name = args[:name]
+		@weight = args[:weight]
+		@height = args[:height]
+		PIRATES << self
+	end
+
+	def self.all
+		PIRATES
+	end
+
 end
