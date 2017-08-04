6dbb1ffbbd95be88595b59da12ae7554dbf67bac
Enoch2k2 <enoch2k2@hotmail.com>
2017-08-04 13:52:51 -0500

    Done.

--- app.rb
+++ app.rb
@@ -3,7 +3,26 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
+    # render the home page
+    get '/' do
+      erb :root
+    end
+
+    # render the new pirate page
+    get '/new' do
+      erb :'pirates/new'
+    end
+
+    # create a new pirate and render the show page
+    post '/pirates' do
+      @pirate = Pirate.new(params[:pirate])
+
+      params[:pirate][:ships].each do |ship_details|
+        @pirate.ships.push(Ship.new(ship_details))
+      end
+      
+      erb :'pirates/show'
+    end
 
   end
 end
