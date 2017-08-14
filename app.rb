d40d262c0a9ebdfa0ab9ab37702fb694d8da9bef
psnider30 <pbsnider@gmail.com>
2017-08-14 14:00:13 -0700

    Lab more than half completed

--- app.rb
+++ app.rb
@@ -3,7 +3,23 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
+    get '/' do
+      erb :root
+    end
+
+    get '/new' do
+
+      erb :'pirates/new'
+    end
+
+    post '/pirates' do
+      binding.pry
+      @pirate = Pirate.new(params)
+      @ship_1 = Ship.new(params)
+      @ship_2 = Ship.new(params)
+      erb :'pirates/show'
+    end
+
 
   end
 end
--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,23 @@
 class Pirate
+
+  attr_accessor :name, :weight, :height
+
+  PIRATES = []
+
+  def initialize(params)
+    @name = params[:name]
+    @weight = params[:weight]
+    @heigth = params[:heigth]
+
+    PIRATES << self
+  end
+
+  def self.all
+    PIRATES
+  end
+
+  def self.clear
+    PIRATES.clear
+  end
+
 end
