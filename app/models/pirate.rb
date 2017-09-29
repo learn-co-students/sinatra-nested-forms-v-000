76a443ac4b3148c9370ccc698fc2d86eac49f8c9
Derek  Pelt <derekpelt@gmail.com>
2017-09-29 06:16:14 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,16 @@
 class Pirate
+  attr_accessor :name, :weight, :height
+
+  @@pirates = []
+
+  def initialize(name, weight, height)
+    @name, @weight, @height = name, weight, height
+    @@pirates << self
+  end
+
+  def self.all
+    @@pirates
+  end
+
+
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,21 @@
 class Ship
+  attr_accessor :name, :type, :booty
+
+  @@ships = []
+
+  def initialize(name, type, booty)
+    @name, @type, @booty = name, type, booty
+    @@ships << self
+  end
+
+  def self.all
+    @@ships
+  end
+
+  def self.clear
+    @@ships.clear
+  end
+
+
+
 end
