388c5364dcf979e9d984ce1671de81daafc655ac
Victoria Sowell <tpms123@gmail.com>
2017-10-06 04:10:10 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,18 @@
 class Pirate
+  attr_accessor :name, :weight, :height
+
+  @@pirate = []
+
+  def initialize(params)
+    @name = params[:name]
+    @weight = params[:weight]
+    @height = params[:height]
+    @@pirate << self
+  end
+
+  def self.all
+    @@pirate
+  end
+
+
 end
