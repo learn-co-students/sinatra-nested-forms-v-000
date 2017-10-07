65c5a17eadd74f9903699b38c7d0a8e0dc957bab
Elvin <Dimix181@Gmail.com>
2017-10-07 13:40:46 -0400

    removed Gemfile.lock and regenerated, created pirate and ships models

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,15 @@
 class Ship
+
+  attr_accessor :type, :booty
+  @@all = []
+
+  def self.all
+      @@all
+  end
+
+  def initialize(params)
+    @type  = params[:pirate][:type]
+    @booty = params[:pirate][:booty]
+  end
+
 end
