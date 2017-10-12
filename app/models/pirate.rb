b29e55aa422af5124e2908a48cb70b3bc7dab0b5
Terry-Thompson <terry.lyn.thompson@gmail.com>
2017-10-11 19:20:59 -0500

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,13 @@
 class Pirate
+attr_accessor :name, :weight, :height, :ships
+@@all = []
+
+	def initialize(params)
+	  params.each {|k,v| self.send("#{k}=", v)}
+	  	  @@all << self
+	end
+	
+	def self.all
+	  @@all
+	end
 end
