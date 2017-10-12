abbe0478e61b5d62b4e6c26acefe1bc87cc17f1e
Brian Bytnar <bytnarb2@gmail.com>
2017-10-11 21:21:31 -0400

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,21 @@
 class Pirate
+  attr_accessor :name, :weight, :height
+
+  @@pirates = []
+
+  def initialize(args)
+    @name = args[:name]
+    @weight = args[:weight]
+    @height = args[:height]
+    @@pirates << self
+  end
+
+  def self.all
+    @@pirates
+  end
+
+  def self.clear
+    self.all.clear
+  end
+
 end
