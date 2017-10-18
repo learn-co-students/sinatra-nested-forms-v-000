f1f5b5e783744f7ec492d4bbd1c20afc834c4e32
github username <github email address>
2017-10-18 17:45:26 -0500

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,18 @@
-class Pirate
+class Pirate #has many ships
+  attr_accessor :name, :weight, :height, :ships
+  @@all = []
+
+  def initialize(pirate_details) #passes in params hash
+
+    @name = pirate_details[:name]
+    @height = pirate_details[:height]
+    @weight = pirate_details[:weight]
+    @ships = pirate_details[:ships]
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
 end
