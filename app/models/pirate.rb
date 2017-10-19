8b82ab9dcaffb23da7cc612ce11d71f79d7531d0
Yvonne Dreptate <YvonneDreptate@gmail.com>
2017-10-19 15:00:14 -0400

    create ship and pirate classes

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,17 @@
 class Pirate
+  attr_accessor :name, :weight, :height
+
+  @@pirates = []
+
+  def initalize(name,weight, height)
+    @name = name
+    @weight = weight
+    @height = height
+    @@all << self
+  end
+
+  def self.all
+    @@pirates
+  end
+
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,22 @@
 class Ship
+  attr_accessor :name, :type, :booty
+
+  @@ships = []
+
+  def initalize(name, type, booty)
+    @name = name
+    @type = type
+    @booty = booty
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear
+    @@all = []
+  end
+
+
 end
