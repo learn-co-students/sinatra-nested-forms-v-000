d9e2992a628105b07217d85cf97a2ac7f9c1f78d
chrisjamesr <chrisjames.r@gmail.com>
2017-10-23 18:27:30 -0400

    add ActiveRecord adapter for db persistence

--- environment.rb
+++ environment.rb
@@ -6,4 +6,9 @@ Bundler.require(:default, ENV['SINATRA_ENV'])
 require 'sinatra/base'
 require 'sinatra/reloader'
 
+ActiveRecord::Base.establish_connection (
+  :adapter => 'sqlite3',
+  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+  )
+
 require_all './app'
