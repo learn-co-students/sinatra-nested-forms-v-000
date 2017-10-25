0f84f723b15db3e885f4852aa8c60fe9fbbd4de9
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-10-25 18:03:30 -0400

    done

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,17 @@
 class Pirate
+
+  attr_reader :name, :weight, :height
+
+  @@pirates = []
+  def initialize(params)
+    @name = params[:name]
+    @weight = params[:weight]
+    @height = params[:height]
+    self.class.all << self
+  end
+
+  def self.all
+    @@pirates
+  end
+
 end
