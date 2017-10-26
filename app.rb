295fb3fec62486f12d6a51ce20315ea82b2aaef8
kyle-poretto <kyleporetto@gmail.com>
2017-10-26 15:50:17 -0400

    Done.

--- app.rb
+++ app.rb
@@ -3,20 +3,29 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
     get '/' do
-      erb :root
+      erb :root   # the controller is directing you towards the root
     end
 
-    get '/new' do
+    get '/new' do   # nav to new. Controller goes to view for pirates
       erb :'pirates/new'
     end
 
     post '/pirates' do
-      @pirate = Pirate.new(params[:pirate])
+
+      @pirate = Pirate.new(params[:pirate]) # through HTTP requests, 
+      #we're posting data in the hash "params"
+      # we're getting inside the object we want, and make that our params
+      #This then talk to pirate model
+      #
 
       params[:pirate][:ships].each do |details|
         Ship.new(details)
+
+        #then intiates all the ships
       end
       @ships = Ship.all
+
+      #return all the ships so we can cycle through them and display them
 
       erb :'pirates/show'
     end
