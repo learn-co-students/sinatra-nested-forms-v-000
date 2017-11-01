9a9a74c8a160451a44357a0b388b9c9d6100f0af
John Tuttle <thejwt@gmail.com>
2017-11-01 21:37:59 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,21 @@
+require 'pry'
 class Pirate
+
+  attr_accessor :name, :weight, :height, :ships
+
+  @@all = []
+
+  def initialize(params)
+    @name = params[:name]
+    @weight = params[:weight]
+    @height = params[:height]
+
+    @@all << self
+
+  end
+
+  def self.all
+    @@all
+  end
+
 end
