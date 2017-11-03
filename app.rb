011b2e2ac4a439615ae4f6e52998122675414802
Denis Bondin <dsbondin@gmail.com>
2017-11-03 23:38:02 +0000

    .

--- app.rb
+++ app.rb
@@ -3,7 +3,24 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
+    get '/' do
+      erb :root
+    end
+
+    get '/new' do
+      erb :'/pirates/new'
+    end
+
+    post '/pirates' do
+      @pirate = Pirate.new
+      @pirate.name = params[:pirate][:name]
+      @pirate.weight = params[:pirate][:weight]
+      @pirate.height = params[:pirate][:height]
+
+
+
+      erb :'pirates/show'
+    end
 
   end
 end
--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,12 @@
 class Pirate
+  attr_accessor :name, :weight, :height
+  @@all = []
+
+  def initialize
+  end
+
+  def self.all
+    @@all
+  end
+
 end
