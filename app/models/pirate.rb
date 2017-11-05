05bb02c398dff602aeaab78d7a42aad9a0d5b9fc
Nicholas Halloran <nmhalloran@hotmail.com>
2017-11-05 01:34:43 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,9 @@
 class Pirate
+  attr_accessor :name, :weight, :height, :ships
+
+  def initialize(name=nil, weight=nil, height=nil)
+    @name = name
+    @weight = weight
+    @height = height
+  end
 end
