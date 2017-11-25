ef4b9037ef1bc2261f938640a07ad01871c61ae1
Aly Alidina <aly_alidina@hotmail.com>
2017-11-25 21:38:57 +0000

    Done.

--- app.rb
+++ app.rb
@@ -4,6 +4,21 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get '/' do
+      "Welcome to the Nested Forms Lab! let's navigate to the '/new'"
+    end
+
+    get "/new" do
+      erb :"pirates/new"
+    end
+
+    post '/pirates' do
+      @pirate = Pirate.new(params[:pirate])
+      @ships = params[:pirate][:ships].map do |details|
+        Ship.new(details)
+      end
+      erb :"pirates/show"
+    end
 
   end
 end
--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,20 @@
 class Pirate
+  attr_accessor :name, :weight, :height
+
+  @@all = []
+
+  def initialize (args)
+    @name = args[:name]
+    @weight = args[:weight]
+    @heigth = args[:heigth]
+    @@all << self
+  end
+
+  def all
+    @@all
+  end
+
+  def self.clear
+    self.all.clear
+  end
 end
