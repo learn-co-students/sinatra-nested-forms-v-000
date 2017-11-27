3436a8d7baf709c4fb916173372ee5aebd685d1e
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2017-11-27 13:31:06 -0500

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,18 @@
 class Pirate
+
+  @@all = [ ]
+
+  attr_accessor :name, :weight, :height
+
+  def initialize(details)
+    @name = details[:name]
+    @weight = details[:weight]
+    @height = details[:height]
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
 end
