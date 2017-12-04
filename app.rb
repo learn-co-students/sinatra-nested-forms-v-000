80d14f3d992f992d33407381b0a3a02bbec1de61
Mel Rudick <melissarudick@MacBook-Air.local>
2017-12-04 11:41:07 -0600

    Done.

--- app.rb
+++ app.rb
@@ -3,7 +3,16 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
+    get "/" do
+      erb :root
+    end
 
+    get "/new" do
+      erb :pirates/new
+    end
+
+    get "/post" do
+      erb :show
+    end
   end
 end
