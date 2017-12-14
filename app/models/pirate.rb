f89a1f199d2424038a929cc5c72e9d8550d6dffa
jessmitch42 <jessicamitch42@gmail.com>
2017-12-14 13:59:13 -0500

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,7 +1,7 @@
 class Pirate
   # Create two classes, a Pirate class and a Ship class. Pirates should have a name, weight, and height. You will also need a class method that returns pirates the pirates.
 
-  attr_accessor :name, :weight, :height, :ships
+  attr_accessor :name, :weight, :height
 
   @@pirates = []
 
