c013f2fcdc66852bba87cfe79636675fb30d1521
chrissyhunt87 <chrissy.hunt@gmail.com>
2017-12-15 16:03:48 -0500

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,17 @@
 class Ship
+  attr_accessor :name, :type, :booty
+
+  SHIPS = []
+
+  def initailize
+    SHIPS << self
+  end
+
+  def self.all
+    SHIPS
+  end
+
+  def self.clear
+    SHIPS.clear
+  end
 end
