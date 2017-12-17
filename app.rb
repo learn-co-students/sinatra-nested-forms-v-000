c0f735066049e5b88b40118a9d19809efbd92517
Brittany Hartmire <bkhartmire@gmail.com>
2017-12-17 07:38:14 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,8 +2,13 @@ require './environment'
 
 module FormsLab
   class App < Sinatra::Base
+    get '/' do
+      erb :new
+    end
 
-    # code other routes/actions here
+    post '/pirates' do
+      erb :show
+    end
 
   end
 end
