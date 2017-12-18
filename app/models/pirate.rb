d96c45db12265b0391d1281c5a147f8168abb4e5
dbfarms <matt.hunger@gmail.com>
2017-12-18 22:54:25 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,20 @@
 class Pirate
+
+  attr_accessor :name, :weight, :height 
+  
+  @@all = [] 
+  
+  def initialize(params)  #(name, weight, height)
+    @name = params[:name]
+    @weight = params[:weight] 
+    @height = params[:height]
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  
+
 end
