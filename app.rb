6efc4ef7894b8be5ebd45242fc20390624176806
roitblatari <roitblatari@gmail.com>
2017-12-31 23:28:09 -0500

    Done.

--- app.rb
+++ app.rb
@@ -11,8 +11,16 @@ module FormsLab
     get '/new' do
       erb :"pirates/new"
     end
-
-    post '/pirates'do
+  
+    post '/pirates' do 
+    
+      
+      @pirate = Pirate.new(params["pirate"])
+      # binding.pry
+      params["pirate"]["ships"].each{ |details| Ship.new(details)}
+    
+      @all_ships = Ship.all
+        # binding.pry
       erb :"pirates/show"
     end
   
--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,11 +1,12 @@
 class Pirate
   attr_accessor :name, :weight, :height
   @@all = []
-  def initialize(name, weight, height)
-    @name = name 
-    @weight = weight  
-    @height = height
-    self.all << self
+  def initialize(params)
+    # name , weight, height
+    @name = params[:name] 
+    @weight = params[:weight]  
+    @height = params[:height]
+    @@all << self
   end
 
   def self.all
