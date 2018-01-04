720ace8ac52ab10829761c3f0c3e0678b3ba8439
Richard Burd <richardburdx@yahoo.com>
2018-01-04 20:36:38 +0000

    emptied pirate & ship models then rolledback migrations

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,36 +1,3 @@
-require './config/environment' # for some reason, I'm not able to run the
-                               # migrations without this requirement block.
-=begin
-when I run the rspec/capybara test suite I get the following error message:
-    POST '/pirates' - returns a 200 status code (FAILED - 1)
-Although this error message is essentially saying the '/pirates' page will
-not load, I have checked the web app several times and it seems to work just fine
+class Pirate
 
-the rspec/capybara error message above will be resolved if I do the following:
-    1.) delete the "ActiveRecord::Base" and "has_many :ships" blocks from this file
-    2.) delete the equivelant blocks in the ship.rb model in this same directory
-I'm guessing the rspec/capybara error message and migration problem are related but
-I honestly have no idea.
-
-If any of the FlatironSchool coaches could download my workspace and attempt to
-debug this, such efforts would be greatly appreciated.
-=end
-
-class Pirate < ActiveRecord::Base
-  has_many :ships
-  attr_reader :name, :height, :weight
-
-  @@pirates = []
-
-  def initialize(paramz)
-    @name = paramz[:name]
-    @height = paramz[:height]
-    @weight = paramz[:weight]
-
-    @@pirates << self
-  end
-
-  def self.all
-    @@pirates
-  end
-end
+end 
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,21 +1,3 @@
-class Ship < ActiveRecord::Base
-  belongs_to :pirate
-  attr_reader :name, :type, :booty
+class Ship
 
-  @@ships = []
-
-  def initialize(args)
-    @name = args[:name]
-    @type = args[:type]
-    @booty = args[:booty]
-    @@ships << self
-  end
-
-  def self.all
-    @@ships
-  end
-
-  def self.clear
-    @@ships = []
-  end
-end
+end 
