1b961ce3a793054250d40fc4f30165cb564b59f4
Richard Burd <richardburdx@yahoo.com>
2018-01-04 18:53:56 +0000

    added notes to pirate.rb on current bugs

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,4 +1,12 @@
-require './config/environment' #figure out why this doesn't work without this
+require './config/environment' # for some reason, I'm not able to run the migrations without this requirement block.
+                               # when I run the rspec/capybara test suite I get the following error message:
+                               # POST '/pirates' - returns a 200 status code (FAILED - 1)
+
+                               # the rspec/capybara error message above will be resolved if I do the following:
+                               # 1.) delete the "ActiveRecord::Base" and "has_many :ships" blocks from this file
+                               # 2.) delete the equivelant blocks in the ship.rb model in this same directory
+                               # I'm guessing the rspec/capybara error message and migration problem are related...
+                               # ...but I honestly have no idea.
 
 class Pirate < ActiveRecord::Base
   has_many :ships
