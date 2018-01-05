d9bd53c159453d3a5525fe6ee3d23943311d4989
Richard Burd <richardburdx@yahoo.com>
2018-01-05 00:34:46 +0000

    created new application controller and view folder

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,19 @@
+require './config/environment'
+# This is the second controller of the program
+class ApplicationController < Sinatra::Base
+
+  configure do
+    # For some reason I need this cause
+    # Sinatra is looking for the views folder in the controllers folder
+    set :views, "app/views"
+  end
+
+  get '/pirates' do
+    @pirates = Pirate.all
+    @ships = Ship.all
+    erb :'pirates'
+  end
+
+  get '/pirates/:id' do
+  end
+end
--- config.ru
+++ config.ru
@@ -1,2 +1,7 @@
 require './app'
-run FormsLab::App
+
+# This was the original controller with this workspace:
+# run FormsLab::App
+
+# This is the new one built from scratch just to play around & see how things work
+ run ApplicationController
