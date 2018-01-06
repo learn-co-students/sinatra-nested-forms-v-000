999c5bbb46580c2e4d8f5f98b491e77cb87b8ede
Jacqueline Lee <anginblu@gmail.com>
2018-01-06 06:05:02 +0000

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,20 @@
 class Ship
+  attr_reader :name, :type, :booty
+
+ PIRATES = []
+
+  def initialize(params)
+    @name = params[:name]
+    @type = params[:type]
+    @booty = params[:booty]
+    PIRATES << self
+  end
+
+  def self.all
+    PIRATES
+  end
+
+  def self.clear
+    PIRATES.clear
+  end
 end
