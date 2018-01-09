77ab276350ac8950e07469ef5644de87e388efc3
Caroline Shields <caroline.shields@amkor.com>
2018-01-09 23:06:40 +0000

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,20 @@
 class Ship
-end
+
+  attr_accessor :name, :type, :booty
+  Ships = []
+  def initialize(info)
+    @name = info[:name]
+    @type = info[:type]
+    @booty = info[:booty]
+    Ships << self
+  end
+
+  def self.all
+    Ships
+  end
+
+  def self.clear
+    Ships.clear
+  end
+
+end #ends Ship class
