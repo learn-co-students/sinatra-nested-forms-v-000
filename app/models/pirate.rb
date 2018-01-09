ff97e9aa46ce5d112fe4a6a21f3753ca7aa6891d
Jered Sweeney <sweeneyjered@gmail.com>
2018-01-09 14:37:43 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,14 @@
 class Pirate
+  attr_accessor :name, :weight, :height
+  @@pirates = []
+
+  def initialize(arg)
+    @name, @weight, @height = arg[:name], arg[:weight], arg[:height]
+    @@pirates << self
+  end
+
+  def self.all
+    @@pirates
+  end
+
 end
