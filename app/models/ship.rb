085ae605e34e48266e84cebde0773dff6ef1479c
Frederico  Morato <freddymorato@gmail.com>
2018-01-12 22:11:56 +0000

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,23 @@
+
 class Ship
+
+    attr_accessor :name, :type, :booty
+
+    SHIPS = []
+
+    def initialize (properties)
+        @name = properties[:name]
+        @type = properties[:type]
+        @booty = properties[:booty]
+        SHIPS << self
+    end
+
+    def self.all
+        SHIPS
+    end
+
+    def self.clear
+        self.all.clear
+    end
+
 end
