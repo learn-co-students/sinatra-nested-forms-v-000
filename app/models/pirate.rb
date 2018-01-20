d77c0ed7b7144ebdbbb408abfd48d350ef7d2752
ronald vilorio <soccer23415@gmail.com>
2018-01-20 00:09:54 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,13 @@
 class Pirate
+  attr_accessor :name,:weight,:height
+  @@all = []
+  def initialize(name=nil,weight=nil,height=nil)
+    @name = name
+    @weight = weight
+    @height = height
+    @@all << self
+  end
+  def self.all
+    @@all
+  end
 end
