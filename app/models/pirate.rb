a713edf7e96d1da95931ec562fafc722ef7b8b00
lizbur10 <liz.burton147@gmail.com>
2018-01-22 09:37:32 -0500

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,16 @@
 class Pirate
+    attr_accessor :name, :weight, :height
+
+    @@all = []
+
+    def initialize(arghs)
+        @name = arghs[:name]
+        @weight = arghs[:weight]
+        @height = arghs[:height]
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
 end
