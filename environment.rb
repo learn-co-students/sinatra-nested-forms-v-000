dd6cd71c0807aa22acdade08ce20cc6c99f3e316
churchwerks <steven.lennis@icloud.com>
2018-01-25 15:12:19 -0600

    Created Tables
    Created Tables and Associated Models.

--- environment.rb
+++ environment.rb
@@ -4,7 +4,7 @@ require 'bundler'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
 ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3"
+  :adapter => "sqlite3",
   :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
 )
 require 'sinatra/base'
