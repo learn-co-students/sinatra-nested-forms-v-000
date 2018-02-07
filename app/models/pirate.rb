b33b1b57a6bab93e1aac9b9dd8500a2b30fb4275
rh24 <ms.rebeccahong@gmail.com>
2018-02-07 13:53:58 -0500

    Add db, Gemfile, Rakefile, environment.rb, and ActiveRecord associations

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,19 +1,20 @@
-class Pirate
-  attr_accessor :name, :weight, :height
-  PIRATES = []
-
-  def initialize(args)
-    @name = args[:name]
-    @weight = args[:weight]
-    @height = args[:height]
-    PIRATES << self
-  end
-
-  def self.all
-    PIRATES
-  end
-
-  def self.clear
-    self.all.clear
-  end
+class Pirate < ActiveRecord::Base
+  has_many :ships
+  # attr_accessor :name, :weight, :height
+  # PIRATES = []
+  #
+  # def initialize(args)
+  #   @name = args[:name]
+  #   @weight = args[:weight]
+  #   @height = args[:height]
+  #   PIRATES << self
+  # end
+  #
+  # def self.all
+  #   PIRATES
+  # end
+  #
+  # def self.clear
+  #   self.all.clear
+  # end
 end
