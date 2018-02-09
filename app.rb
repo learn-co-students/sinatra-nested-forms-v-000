440addde81c21c73dbad598e3eece2edae247103
Ilya Zarembsky <wlycdgrfromflatiron@gmail.com>
2018-02-08 22:00:53 -0500

    Rspeclemented post '/pirates'

--- app.rb
+++ app.rb
@@ -11,5 +11,21 @@ module FormsLab
       erb :"pirates/new"
     end
 
+    post '/pirates' do
+      @pirate = Pirate.new(
+        params[:pirate][:name],
+        params[:pirate][:weight],
+        params[:pirate][:height]
+      )
+
+      @ships = params[:pirate][:ships].collect do |ship_details|
+        Ship.new(
+          ship_details[:name], 
+          ship_details[:type],
+          ship_details[:booty])
+      end
+
+      erb :"pirates/show"
+    end
   end
 end
--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,17 @@
 class Pirate
+    attr_accessor :name, :weight, :height
+
+    PIRATES = []
+
+    def initialize(name, weight, height)
+        self.name = name
+        self.weight = weight
+        self.height = height
+
+        PIRATES << self
+    end
+
+    def self.all
+        PIRATES
+    end
 end
