af1ce33ecc48b86fe182ccbee9da0db03442f984
Sally Zasloff <szasloff@gmail.com>
2018-02-09 01:05:02 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,8 +2,13 @@ require './environment'
 
 module FormsLab
   class App < Sinatra::Base
-
     # code other routes/actions here
 
+    # get '/' do
+    #   erb :new.erb
+    # end
+
+
+  
   end
 end
