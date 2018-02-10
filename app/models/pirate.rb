b9d6851811deac5316b6dafc9c7cd955d18edce5
James Ferguson <ncjamesferguson@gmail.com>
2018-02-10 23:06:24 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,15 @@
 class Pirate
+
+  @@pirates = []
+
+  attr_accessor :name, :weight, :height
+
+  def initialize
+  end
+
+  def self.all
+    @@pirates
+  end
+
+
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,14 @@
 class Ship
+
+  @@ships = []
+
+  attr_accessor :name, :type, :booty
+
+  def initialize
+  end
+
+  def self.all
+    @@ships
+  end
+
 end
