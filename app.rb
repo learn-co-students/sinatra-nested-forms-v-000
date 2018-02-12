3cce1bff693b3a7711bbdb7ca9a863a3f1a62573
Sally Zasloff <szasloff@gmail.com>
2018-02-12 18:48:22 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,14 +1,33 @@
 require './environment'
-
+require 'pry'
 module FormsLab
   class App < Sinatra::Base
     # code other routes/actions here
 
-    # get '/' do
-    #   erb :new.erb
-    # end
+    get '/' do
+      erb :root
+    end
+
+    get '/new' do
+      erb :'pirates/new'
+    end
+
+    post '/pirates' do
+      @pirate = Pirate.new(params[:pirate])
+
+      params[:pirate][:ships].each do |details|
+        Ship.new(details)
+        # binding.pry
+      end
+
+      @ships = Ship.all
 
 
-  
+
+      erb :'pirates/show'
+    end
+
+
+
   end
 end
--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,12 +1,13 @@
+require 'pry'
 class Pirate
   attr_reader :name, :weight, :height
 
   PIRATES = []
 
   def initialize (params)
-    @name = name
-    @weight = weight
-    @height = height
+    @name = params[:name]
+    @weight = params[:weight]
+    @height = params[:height]
     PIRATES<< self
   end
 
