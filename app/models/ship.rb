149c57b6ef4523e2fe7cc2b8e13ba98d7425b422
Alex Crisan <alexcrisan2005@hotmail.com>
2018-02-26 17:14:27 +0000

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,22 @@
+require 'pry'
+
 class Ship
+  attr_accessor :name, :type, :booty
+
+  SHIPS = []
+
+  def intialize(details)
+    @name = details[:name]
+    @type = details[:type]
+    @booty = details[:booty]
+    SHIPS << self
+  end
+
+  def self.all
+    SHIPS
+  end
+
+  def self.clear
+    SHIPS.clear
+  end
 end
