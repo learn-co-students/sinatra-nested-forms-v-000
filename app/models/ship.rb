5a32e70f4fa6127cf16c63f196b0a1c9f5e82f83
Katrina Brinson <kbrinso@gmail.com>
2018-02-28 22:40:17 +0000

    200 success on second GET controller

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,19 @@
 class Ship
+  attr_accessor :name, :type, :booty
+  @@ALLSHIPS = []
+  def initialize(name, type, booty)
+    @name = name
+    @type = type
+    @booty = booty
+    @@ALLSHIPS << self
+  end
+
+  def self.all
+    @@ALLSHIPS
+  end
+
+  def self.clear
+    @@ALLSHIPS = []
+  end
 end
+#name, type, and booty attributes
