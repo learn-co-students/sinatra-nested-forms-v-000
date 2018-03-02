843ba353ec6026ef3d42fb3a5f46e25ba91a45f5
Sean Clifford <sclifford6262@gmail.com>
2018-03-02 04:49:01 +0000

    Done.

--- spec/01_sinatra_forms_spec.rb
+++ spec/01_sinatra_forms_spec.rb
@@ -39,7 +39,7 @@ describe "FormsLab::App" do
       expect(last_response.body).to include("pirate[ships][][type]")
       expect(last_response.body).to include("pirate[ships][][booty]")
     end
-
+ 
     it "renders the second ship's input field for the name, type, and booty attributes on the page" do
       expect(last_response.body).to include("pirate[ships][][name]")
       expect(last_response.body).to include("pirate[ships][][type]")
