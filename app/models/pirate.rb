5f07e5585961e62f5df08497c08e9b72376966bd
github username <github email address>
2018-03-09 14:31:38 -0500

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,19 @@
 class Pirate
+  attr_accessor :name, :weight, :height, :ships
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def self.clear
+    @@all.clear
+  end
+
+  def initialize(attributes={})
+    attributes.each {|key, value| self.send(("#{key}="), value)}
+    @@all << self
+  end
+
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,19 @@
 class Ship
+  attr_accessor :name, :type, :booty
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def self.clear
+    @@all.clear
+  end
+
+  def initialize(attributes={})
+    attributes.each {|key, value| self.send(("#{key}="), value)}
+    @@all << self
+  end
+
 end
