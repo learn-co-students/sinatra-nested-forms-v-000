d2ca63de9f2ddb29f5c768e43eb22ad16117e80b
EC2 Default User <ec2-user@ip-172-31-32-154.us-east-2.compute.internal>
2018-03-19 22:23:41 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,9 +1,26 @@
 require './environment'
+require './app/models/pirates'
+require './app/models/ships'
 
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
-
+    get '/' do
+      erb :root
+    end
+    
+    get '/new' do
+      erb :'pirates/new'
+    end
+    
+    post '/pirates' do
+      @nu_pirate = Pirate.new(params[:pirate][:name], params[:pirate][:weight], params[:pirate][:height])
+      @nu_ship1 = Ship.new(params[:pirate][:ships][0][:name], params[:pirate][:ships][0][:type], params[:pirate][:ships][0][:booty])
+      if params[:pirate][:ships].length > 1
+        @nu_ship2 = Ship.new(params[:pirate][:ships][1][:name], params[:pirate][:ships][1][:type], params[:pirate][:ships][1][:booty])
+      end
+      erb :'/pirates/show'
+    end
+    
   end
 end
