2218ebcbb42b97d13748cb07d711408666fb1c31
Lindsay Dennis <lindsay.c.dennis@gmail.com>
2018-03-20 21:28:23 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,17 @@
 class Pirate
+  attr_accessor :name, :weight, :height 
+  
+  def initialize(params)
+    @name = params[:name]
+    @weight = params[:weight]
+    @height = params[:height]
+    @@all << self
+  end
+  
+  @@all = []
+  
+  def self.all 
+    @@all
+  end 
+  
 end
