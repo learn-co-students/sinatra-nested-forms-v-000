aa0ab738e6639f411c905598c8da8b94b2bb22d8
Brennan Heisler <learn@heislercreative.com>
2018-03-20 01:06:55 +0000

    Done.

--- app.rb
+++ app.rb
@@ -6,21 +6,21 @@ module FormsLab
     get '/' do
       erb :root
     end
-    
+
     get '/new' do
       erb :'pirates/new'
     end
-    
+
     post '/pirates' do
       info = params[:pirate]
       @pirate = Pirate.new(name: info[:name], weight: info[:weight], height: info[:height])
-      
+
       info[:ships].each do |details|
         ship = Ship.new(details)
         ship.pirate = @pirate
         ship.save
       end
-      
+
       erb :'pirates/show'
     end
 
