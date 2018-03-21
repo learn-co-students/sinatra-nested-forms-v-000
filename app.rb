4db8ed964588a8d35d9b6436466727c5fbb9bd1d
Aubrey Neimeier <neimeier.1@osu.edu>
2018-03-21 17:59:20 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,8 +2,29 @@ require './environment'
 
 module FormsLab
   class App < Sinatra::Base
+    get '/' do
+      erb :root
+    end
 
-    # code other routes/actions here
+    get '/new' do
+      #points to an erb that holds the form
+      "Hello World"
+      erb :"pirates/new"
+    end
+
+    post '/new' do
+      #points to an erb that displays the info.
+      #several instance variables will be declared here
+      @new_pirate = Pirate.new(params[:pirate])
+
+      params[:pirate][:ships]. each do |details|
+        Ship.new(details)
+      end
+
+      @ships = Ship.all
+      erb :"pirates/show"
+    end
+
 
   end
 end
