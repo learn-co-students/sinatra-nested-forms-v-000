99d6d90bf03a56244550302d00c9b5e8dc7588d1
EC2 Default User <ec2-user@ip-172-31-14-103.us-east-2.compute.internal>
2018-03-22 17:48:34 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,8 +2,35 @@ require './environment'
 
 module FormsLab
   class App < Sinatra::Base
+    configure do
+      set :views, ['views', 'views/pirates']
+    end
 
+    helpers do
+      def find_template(views, name, engine, &block)
+        views.each { |v| super(v, name, engine, &block) }
+      end
+    end
+    
     # code other routes/actions here
-
+    get '/' do
+      erb :root
+    end
+    
+    get '/new' do
+      erb :new
+    end
+    
+    post '/pirates' do
+      @pirate = Pirate.new(params[:pirate])
+ 
+      params[:pirate][:ships].each { |details|
+        Ship.new(details)
+      }
+ 
+      @ships = Ship.all
+    
+      erb :show
+    end
   end
 end
