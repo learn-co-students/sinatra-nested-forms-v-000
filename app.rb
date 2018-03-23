c6fb40b291bcbd0956beca9f57c9465fb3832571
Eden Mesfun <emesfun@gmail.com>
2018-03-23 19:56:14 +0000

    Done.

--- app.rb
+++ app.rb
@@ -13,8 +13,6 @@ module FormsLab
     end
 
     post '/pirates' do
-
-
       @pirate = Pirate.new(params[:pirate][:name], params[:pirate][:weight], params[:pirate][:height])
       @ship1 = Ship.new(params[:pirate][:ships][0][:name], params[:pirate][:ships][0][:type], params[:pirate][:ships][0][:booty])
       @ship2 = Ship.new(params[:pirate][:ships][1][:name], params[:pirate][:ships][1][:type], params[:pirate][:ships][1][:booty])
