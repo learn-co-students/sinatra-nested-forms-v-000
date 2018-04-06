2a9c4c3e700d70e54c9ce13ee1becd68d18496c8
britt <britt.cumens@gmail.com>
2018-04-06 12:18:39 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,8 +2,24 @@ require './environment'
 
 module FormsLab
   class App < Sinatra::Base
+    get '/' do
+      erb :root
+    end
 
-    # code other routes/actions here
+    get '/new' do
+      erb :'pirates/new'
+    end
+
+    post '/pirates' do
+      #params to instance variables?
+      # raise params.inspect
+      # binding.pry
+      @pirate = Pirate.new(params[:pirate])
+      @ship_1 = Ship.new(params[:pirate][:ships].first)
+      @ship_2 = Ship.new(params[:pirate][:ships].last)
+
+      erb :'pirates/show'
+    end
 
   end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,22 @@
 class Ship
+  attr_accessor :name, :type, :booty
+
+  SHIPS = []
+
+  def initialize(args)
+    #What will be passed in here?  A nested form or just a regular hash?
+    @name = args[:name]
+    @type = args[:type]
+    @booty = args[:booty]
+
+    SHIPS << self
+  end
+
+  def self.all
+    SHIPS
+  end
+
+  def self.clear
+    SHIPS.clear
+  end
 end
