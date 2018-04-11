48139ca597f95cbc0ed438a354f3ea6414115332
Sam. O <samrey2018@gmail.com>
2018-04-11 18:33:58 +0000

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,40 @@
 class Ship
+
+  attr_reader :name, :type, :booty
+
+SHIP = []
+
+def initialize(params)
+  @name =  params[:name]
+  @type= params[:type]
+  @booty= params[:booty]
+ SHIP << self
+end
+
+def self.all
+  SHIP
+
+end
+
+
+attr_reader :name, :weight, :height
+
+PIRATES = []
+
+def initialize(params)
+  @name =  params[:name]
+  @weight= params[:weight]
+  @height= params[:height]
+ PIRATES << self
+end
+
+def self.all
+  PIRATES
+end
+
+  def self.clear
+    PIRATES.clear
+
+  end
+
 end
