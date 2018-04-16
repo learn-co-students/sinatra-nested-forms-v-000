99cfb3d65787e2fbc81eed0f10f1e0d8423599f4
Frank McCallion <fkmccallion@gmail.com>
2018-04-16 15:08:28 +0000

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,22 @@
 class Ship
+
+  attr_accessor :name, :type, :booty
+
+  @@all = []
+
+  def initialize(params) # Pass in ship hash and assign attributes to the ship.
+    @name = params[:name]
+    @type = params[:type]
+    @booty = params[:booty]
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear
+    @@all = []
+  end
+
 end
