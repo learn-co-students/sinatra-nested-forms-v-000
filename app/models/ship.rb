af15ea8258a8458f30f28fd45b2ea440e0df2290
Michael St. Marie <michael.landrum.st.marie@gmail.com>
2018-04-18 16:11:04 +0000

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,24 @@
 class Ship
+  attr_accessor :name, :type, :booty
+
+  ALL = []
+
+  def initialize (name, type, booty)
+    @name = name
+    @type = type
+    @booty = booty
+
+    ALL << self
+  end
+
+  def self.all
+    ALL
+  end
+
+  def self.clear
+    ALL.delete!
+  end
+
+  #returns all the ships and a class method `.clear` that deletes all ships.
+
 end
