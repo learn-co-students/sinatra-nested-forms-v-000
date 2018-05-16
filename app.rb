910813ed4a9afa1d9f1fd1c9a2c9d7a600cd0024
Emily Zhao <tong.zhao.92@gmail.com>
2018-05-16 20:48:06 +0000

    Done.

--- app.rb
+++ app.rb
@@ -3,7 +3,13 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
+    get '/new' do
+      erb :'pirates/new'
+    end
+
+    post '/pirates' do
+      erb :'pirates/show'
+    end
 
   end
 end
