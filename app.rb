726b9cb4d0cebb438dd50c76b7a46f406ea46bb9
Annie Bourke <aabourke@gmail.com>
2018-05-18 23:45:04 +0000

    Done.

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ module FormsLab
       erb :new
     end
 
-    post '/show' do
+    post '/pirates' do
       @pirate = Pirate.new(params[:pirate])
 
       params[:pirate][:ships].each do |details|
