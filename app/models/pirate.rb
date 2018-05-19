02af15cbaef5126316c67586f5a262830a36cbcf
Annie Bourke <aabourke@gmail.com>
2018-05-19 00:58:59 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,17 +1,17 @@
 class Pirate
   attr_reader :name, :weight, :height
 
-  pirates_array = []
+  @@pirates_array = []
 
   def initialize(params)
     @name = params[:name]
     @weight = params[:weight]
     @height = params[:height]
-    pirates_array << self
+    @@pirates_array << self
   end
 
   def self.all
-    pirates_array
+    @@pirates_array
   end
 
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,21 +1,21 @@
 class Ship
   attr_reader :name, :type, :booty
 
-  ships_array = []
+  @@ships_array = []
 
   def initialize(params)
     @name = params[:name]
     @type = params[:type]
     @booty = params[:booty]
-    ships_array << self
+    @@ships_array << self
   end
 
   def self.all
-    ships_array
+    @@ships_array
   end
 
   def self.clear
-    ships_array.clear
+    @@ships_array.clear
   end
 
 end
