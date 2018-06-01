a694b8acd39864086e4ab29962d4c71967ff3b9d
Arye Dov Eidelman <aryedov.e@gmail.com>
2018-06-01 13:48:41 -0400

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,18 @@
 class Pirate
+  attr_accessor :name, :weight, :height
+
+  @@all = []
+
+  def initialize(params)
+    params.each{|k, v| self.send("#{k}=", v)}
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  # def self.clear
+  #   @@all = []
+  # end
 end
