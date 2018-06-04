4d37c0036cd083c023fca86c9f6fb3bef1c78940
David Markel <davidgmarkel@gmail.com>
2018-06-04 17:41:02 +0000

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,20 @@
 class Ship
+
+@@ships = []
+
+  def initialize(params)
+    @name = params[:pirate][:ship][:name]
+    @type = params[:pirate][:ship][:type]
+    @booty = params[:pirate][:ship][:booty]
+    @@ships << self
+  end
+
+  def self.all
+    @@ships
+  end
+
+  def self.clear
+    @@ships.clear
+  end
+
 end
