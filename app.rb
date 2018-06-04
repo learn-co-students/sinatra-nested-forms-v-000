21b1ba9dcc5bb39dae8540f551f01d073ae3a565
Samantha Garcia <samantha.marie.garcia@gmail.com>
2018-06-04 15:39:37 +0000

    Done.

--- app.rb
+++ app.rb
@@ -10,16 +10,16 @@ module FormsLab
       erb :'pirates/new'
     end
 
-    # post '/pirates' do
-    #   @pirate = Pirate.new(params[:pirate])
-    #
-    #   params[:pirate][:ships].each do |details|
-    #     Ship.new(details)
-    #   end
-    #   @ships = Ship.all
-    #
-    #   erb :'pirates/show'
-    # end
+    post '/pirates' do
+      @pirate = Pirate.new(params[:pirate])
+
+      params[:pirate][:ships].each do |details|
+        Ship.new(details)
+      end
+      @ships = Ship.all
+
+      erb :'/pirates/show'
+    end
 
   end
 end
