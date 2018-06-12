80233c11241ada496a986561487e06febc9fae68
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-06-12 14:23:50 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,9 +1,18 @@
 require './environment'
+require_relative 'models/pirate.rb'
+require_relative 'models/ship.rb'
 
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
+   get '/' do
+     erb :new
+   end
+   
+   post '/pirates' do 
+     
+     erb :show
+   end
 
   end
 end
---
+++ models/pirate.rb
@@ -0,0 +1,19 @@
+class Pirate
+  
+  attr_accessor :name, :weight, :height
+  
+  @@all = []
+  
+  def initialize(name, weight, height)
+    @name = name
+    @height = height
+    @weight = weight
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  
+end
