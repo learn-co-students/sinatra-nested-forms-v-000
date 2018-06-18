898f1dcae35430147bfed5f107ec1d8b710fdf6f
Suchitra Naik <susanaik@gmail.com>
2018-06-18 23:35:56 +0000

    Done.

--- app.rb
+++ app.rb
@@ -4,6 +4,29 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get '/' do
+        erb :"/pirates/index"
+    end
+
+    get '/new' do
+       erb :"/pirates/new"
+    end
+
+    post '/pirates' do
+      raise params.inspect
+      @pirate = Pirate.new(params[:pirate[name]], params[:pirate[weigth]], params[:pirate[height]])
+
+      @ship1 = Ship.new(params[:ship_name_1], params[:ship_type_1], params[:ship_booty_1])
+      @ship1 = Ship.all.first
+
+      @ship2 = Ship.new(params[:ship_name_2], params[:ship_type_2], params[:ship_booty_2])
+      @ship2 = Ship.all.last
+
+      @pirates = Pirate.all
+      @ships = Ship.all
+
+      erb :"/pirates/show"
+    end
 
   end
 end
--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,21 @@
 class Pirate
+  attr_accessor :name, :weight, :height
+  @@pirate = []
+
+  def initialize(name, weight, height)
+    @name = name
+    @weight = weight
+    @height = height
+
+    @@pirate << self
+  end
+
+  def self.all
+    @@pirate
+  end
+
+  def self.clear
+    @@pirate.destroy
+  end
+
 end
