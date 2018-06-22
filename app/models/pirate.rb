ca99042812726096344cfec58f53ae7e876e5d98
ho-tonym <ho.tonym@gmail.com>
2018-06-22 12:43:46 -0400

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,10 @@
 class Pirate
+
+  attr_accessor :name, :weight, :height
+
+  def initialize(params)
+    @name = params[:pirate][:name]
+    @weight = params[:pirate][:weight]
+    @height = params[:pirate][:height]
+  end
 end
