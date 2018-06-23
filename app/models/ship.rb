4cb58481da435bb1ef2e23f01f9b06e7e877e539
Judy McGoldrick <jjeanius@verizon.net>
2018-06-23 04:07:33 +0000

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,20 @@
 class Ship
+  attr_reader :name, :type, :booty
+
+  SHIP = []
+
+  def initalize(args)
+    @name = args[:name]
+    @type = args[:stype]
+    SHIP << self
+  end
+
+  def self.all
+    SHIP
+  end
+
+  def self.clear
+    SHIP.clear
+  end
+
 end
