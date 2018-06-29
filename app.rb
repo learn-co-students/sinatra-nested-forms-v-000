6b58193ed8145a6b3a8dbb4a99c1cdd4fb195b13
taryn yaeger <tsyaeger@gmail.com>
2018-06-29 07:12:35 +0000

    Done.

--- app.rb
+++ app.rb
@@ -5,5 +5,37 @@ module FormsLab
 
     # code other routes/actions here
 
+    get '/' do 
+      erb :"./pirates/index"
+    end
+  
+    get '/new' do
+      erb :"./pirates/new"
+    end
+    
+    post '/pirates' do 
+      
+      # raise params.inspect
+      
+      @pirate = Pirate.new(params[:pirate])
+      ships = params[:pirate][:ships]
+      
+      @ship_name_1 = ships[0][:name]
+      @ship_type_1 = ships[0][:type]
+      @ship_booty_1 = ships[0][:booty]
+      
+            
+      @ship_name_2 = ships[1][:name]
+      @ship_type_2 = ships[1][:type]
+      @ship_booty_2 = ships[1][:booty]
+      
+      "#{@ship_name_1}"
+      
+      erb :"./pirates/show"
+    end
+
   end
 end
+
+
+
