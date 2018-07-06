333953a72e59fedea84c73babd40facba604f5cf
Shirley Collins <shirley@m104.us>
2018-07-06 11:17:09 -0700

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,19 @@
 class Pirate
+  attr_accessor :name, :weight, :height
+
+  @@all_pirates = Array.new
+
+  def initialize(param)
+    @name = param[:name]
+    @weight = param[:weight]
+    @height = param[:height]
+  end
+
+  def self.all
+    @@all_pirates
+  end
+
+  def self.clear
+    @@all_pirates.clear
+  end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,19 @@
 class Ship
+  attr_accessor :name, :type, :booty
+
+  @@all_ships = Array.new
+
+  def initialize(param)
+    @name = param[:name]
+    @type = param[:type]
+    @booty = param[:booty]
+  end
+
+  def self.all
+    @@all_ships
+  end
+
+  def self.clear
+    @@all_ships.clear
+  end
 end
