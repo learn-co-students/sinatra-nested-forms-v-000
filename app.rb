8a39c6ce4a4cac151e84a9a799054726dcd788eb
Dillorom Abdulloeva <dabdulloeva@gmail.com>
2018-07-06 00:21:00 +0000

    Done.

--- app.rb
+++ app.rb
@@ -2,8 +2,17 @@ require './environment'
 
 module FormsLab
   class App < Sinatra::Base
-
+    get '/' do
+      erb :root
+    end
     # code other routes/actions here
+    get '/new' do
+      erb :pirates
+    end
+
+    post '/pirates' do
+      erb :pirates
+    end
 
   end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,21 @@
 class Ship
+  attr_accessor :name, :type, :booty_attributes
+
+  SHIPS = []
+
+  def initialize(atr)
+    @name = art[:name]
+    @type = atr[:type]
+    @booty_attributes = atr[:booty_attributes]
+    SHIPS << self
+  end
+
+  def self.all
+    SHIPS
+  end
+
+  def self.clear
+    self.all.clear
+  end
+
 end
