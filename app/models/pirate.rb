c67c2a88aa3f41a7a235bb16feba7fb7470a9aa0
Brennan Fulmer <brennan.flmr@gmail.com>
2018-07-12 17:10:37 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,5 +1,11 @@
 # have a name, weight, and height. You will also need a class method that returns all
 
 class Pirate
+  attr_accessor :name, :weight, :height
+  @@all = []
+  
+  def self.all 
+    @@all
+  end
   
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,4 +1,4 @@
-have name, type, and booty attributes, as well as a class method .all that returns all the ships and a class method .clear
+# have name, type, and booty attributes, as well as a class method .all that returns all the ships and a class method .clear
 
 class Ship
   
