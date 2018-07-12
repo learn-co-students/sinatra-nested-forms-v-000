54bbfd3d134b4f5847f1477868b45c0796c892e3
Brennan Fulmer <brennan.flmr@gmail.com>
2018-07-12 17:07:22 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,5 @@
+# have a name, weight, and height. You will also need a class method that returns all
+
 class Pirate
+  
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,5 @@
+have name, type, and booty attributes, as well as a class method .all that returns all the ships and a class method .clear
+
 class Ship
+  
 end
