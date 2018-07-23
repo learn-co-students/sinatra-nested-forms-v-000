26add427197b8be98a5296e476180ca80d3ae396
Anu Khambete <anukhambete@gmail.com>
2018-07-23 14:00:22 +0000

    Done.

--- app.rb
+++ app.rb
@@ -4,6 +4,14 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get '/' do
+      "Welcome to the Nested Forms Lab! let's navigate to the '/new'"
+
+    end
+
+    get '/new' do
+      erb :'pirates/new'
+    end
 
   end
 end
