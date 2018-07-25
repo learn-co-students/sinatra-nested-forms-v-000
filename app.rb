5b10f4d66199228ea66af073a5b177c9195cb26b
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-07-25 00:01:58 +0000

    Done.

--- app.rb
+++ app.rb
@@ -4,6 +4,12 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
-
+    get '/' do
+      erb :root
+    end
+    
+    get '/pirates/new' do
+      erb :new
+    end
   end
 end
