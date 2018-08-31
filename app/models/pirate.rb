876087a4937a291dec0cb02a04162f47df4e6c10
Jonathan Chan <johnkchan252@gmail.com>
2018-08-31 00:39:47 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,21 @@
 class Pirate
+  attr_accessor :name, :weight, :height
+  
+  @@all = []
+  
+  def self.all
+    @@all
+  end
+  
+  def self.clear
+    @@all.clear
+  end
+  
+  def initialize(info)
+    @name = info[:name]
+    @weight = info[:weight]
+    @height = info[:height]
+    @@all << self
+  end
+  
 end
