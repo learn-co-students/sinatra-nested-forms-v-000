2fb2d95957113996b63a8f15b669d7629c7979be
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-09-11 17:52:33 +0000

    Done.

--- app.rb
+++ app.rb
@@ -8,14 +8,14 @@ module FormsLab
     end
 
     get '/new' do
-      erb :new
+      erb :"pirates/new"
     end
 
     post '/pirates' do
-      @pirate = Pirate.new(params[:pirate])
-      @ship1 = Ship.new(params[:pirate][:ships][0])
-      @ship2 = Ship.new(params[:pirate][:ships][1])
-      erb :show
+      @pirate = Pirate.new(params["pirate"])
+      @ship1 = Ship.new(params["pirate"]["ships"][0])
+      @ship2 = Ship.new(params["pirate"]["ships"][1])
+      erb :"pirates/show"
     end
   end
 end
