ba390466a47931feaa42747df4546b4c48d86498
Emanuel Bierman <emanuel.bierman@gmail.com>
2018-09-11 17:57:36 +0000

    Done.

--- app.rb
+++ app.rb
@@ -15,6 +15,13 @@ module FormsLab
       @pirate = Pirate.new(params["pirate"])
       @ship1 = Ship.new(params["pirate"]["ships"][0])
       @ship2 = Ship.new(params["pirate"]["ships"][1])
+
+      # this is a way to iterate:
+      # params[:pirate][:ships].each do |details|
+      #   Ship.new(details)
+      # end
+      # @ships = Ship.all
+
       erb :"pirates/show"
     end
   end
