00817c7fe5fcfbc92ea43b104ad3e12f5ac28ec9
Madeline Stark <madeline.stark1@gmail.com>
2018-09-12 20:02:23 +0000

    Done.

--- app.rb
+++ app.rb
@@ -3,7 +3,36 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
+    get '/' do
+      erb :'pirates/index' #default just looks in views
+    end
+
+    get '/new' do
+      erb :'pirates/new'
+    end
+
+    #get '/pirates' do
+      #@pirates = Pirate.all
+      #erb :'pirates/new'
+      #gives all the pirates
+    #end
+
+    #for single pirate:
+      #get '/pirates/:id' do
+
+      #end
+
+    post '/pirates' do
+      @pirate = Pirate.new(params[:pirate]) #storing in variable gives show.erb something to call .name and other methods on
+
+      params[:pirate][:ships].each do |details|
+        Ship.new(details)
+      end
+
+      @ships = Ship.all #can then call on with index number
+
+      erb :'pirates/show'
+    end
 
   end
 end
