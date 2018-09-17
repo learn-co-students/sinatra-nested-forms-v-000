e75c9c1c1ec7f4e4e7b7118a29cc51beafb9eac1
github username <github email address>
2018-09-17 16:29:22 +0200

    Done.

--- app.rb
+++ app.rb
@@ -3,7 +3,38 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
+    get '/pirates' do
+    
+      erb :'pirates/index'
+    end
 
+
+    
+
+    get '/' do
+      erb :root
+    end
+
+    # get '/pirates' do
+    #   erb :'pirates/show'
+    # end
+
+    get '/new' do
+      erb :'pirates/new'
+    end
+
+    post '/pirates' do
+      params["pirate"]["ships"].each do |ship_data|
+        Ship.new(ship_data["name"], ship_data["type"], ship_data["booty"])
+      end
+
+      @pirate = Pirate.new(params["pirate"]["name"], params["pirate"]["height"], params["pirate"]["weight"])
+
+      @ships = Ship.all
+
+      erb :'pirates/show'
+    end
+
+    
   end
 end
