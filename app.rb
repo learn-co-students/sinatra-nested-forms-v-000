7d6881f922f7e1ec830acd4ef3504f1056aafc35
Nathan  Visak <nathan.visak@gmail.com>
2018-09-19 17:45:30 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,9 +1,17 @@
 require './environment'
 
 module FormsLab
+
   class App < Sinatra::Base
 
-    # code other routes/actions here
+    get '/' do
+      "Welcome to the Nested Forms Lab! let's navigate to the '/new'."
+    end
 
-  end
+    get '/new' do
+      erb :'pirates/new'
+    end
+
+end
+
 end
