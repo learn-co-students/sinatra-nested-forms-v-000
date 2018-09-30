6cc3b531e0fc134b13c0c93e9560e50663e31aef
Christia T <christiatidwell@bellsouth.net>
2018-09-30 21:18:57 +0000

    completed lab with persistance

--- app.rb
+++ app.rb
@@ -19,8 +19,21 @@ module FormsLab
       erb :'pirates/show'
     end
 
+
     post '/pirates' do
-      
+      pirate = Pirate.create(name: params[:pirate][:name], height: params[:pirate][:height], weight: params[:pirate][:weight])
+      params[:pirate][:ships].each do |ship_data|
+        ship = Ship.new(ship_data)
+        ship.pirate = pirate
+        ship.save
+      end
+
+      redirect to "/pirates/#{pirate.id}"
+      #please go to localhost:9393/pirates/this pirates id
+    end
+
+    post '/pirates' do
+
     end
 
   end
