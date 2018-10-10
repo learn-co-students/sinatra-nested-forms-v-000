2f8bd8320e7700e5c309fc63dc356c87da2c6fea
Sushi Chanrai <sushi@pathworks.co>
2018-10-10 18:00:33 -0400

    Done.

--- app.rb
+++ app.rb
@@ -3,7 +3,26 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
+    get '/' do
+      erb :root
+    end
+
+    get '/new' do
+      erb :new
+    end
+
+    post '/pirates' do
+      @pirate = Pirate.new(params[:pirate])      #first create a new Pirate using the info stored in params[:student],
+                                                    #which contains the pirates's name, weight, and height and ships
+      params[:pirate][:ships].each do |info|  #we iterate over params[:pirate][:ships], an array containing a series of hashes
+                                                    #that each store individual ship information:
+        Ship.new(info)                         #Use ships values passed into the .each block to create instances of our Ship class.
+      end
+        @ships = Ship.all
+      erb :show
+    end
+
+
 
   end
 end
