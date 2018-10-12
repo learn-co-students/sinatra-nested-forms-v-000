2fb9c7860a3caa0964a3705c20a303bd5e11f245
Bekah Hawrot Weigel <rhawrot@gmail.com>
2018-10-12 13:13:02 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,19 @@
 class Pirate
+ attr_accessor :name, :weight, :height
+  
+  STUDENTS = []
+  
+  def initialize(params)
+    @name = params[:name]
+    @weight = params[:weight]
+    @height = params[:height]
+   STUDENTS << self
+  end
+  
+  def self.all
+    STUDENTS
+  end
+  
+ 
+
 end
