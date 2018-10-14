fdf1f0d2f28d486d89c2291fb871ee4e353ca859
Adam Barton <adam.barton333@gmail.com>
2018-10-14 18:21:39 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,15 @@
 class Pirate
+
+  attr_accessor :name, :weight, :height
+
+  PIRATES = []
+
+  def initialize
+    PIRATES << self
+  end
+
+  def self.all?
+    PIRATES
+  end
+
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,19 @@
 class Ship
+
+  attr_accessor :name, :type, :booty
+
+  SHIPS = []
+
+  def initialize
+    SHIPS << self
+  end
+
+  def self.all?
+    SHIPS
+  end
+
+  def self.clear
+    SHIPS.clear
+  end
+
 end
