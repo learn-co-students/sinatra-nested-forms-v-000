7adce26258e56bff1d1df47711c9fcb354cdd211
Valerie Lynie McClimon <vmcclimon33@yahoo.com>
2018-10-23 00:05:33 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,17 @@
 class Pirate
+  attr_reader :name, :weight, :height
+  
+  PIRATES = []
+  
+  def initialize(args)
+    @name = args[:name]
+    @type = args[:type]
+    @booty = args[:booty]
+    PIRATES << self
+  end
+  
+  self.all 
+    PIRATES
+  end
+  
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,21 @@
 class Ship
+  attr_reader :name, :type, :booty
+  
+  SHIPS = []
+  
+  def initialize(params)
+    @name = params[:name]
+    @type = params[:type]
+    @booty = params[:booty]
+    SHIPS << self
+  end
+  
+  self.all
+    SHIPS
+  end
+  
+  self.clear
+    SHIPS.clear
+  end
+  
 end
