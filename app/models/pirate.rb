a378902f69564b77da7ff308e66fd41b535ffca3
Justine B <justinegayton@gmail.com>
2018-10-30 10:57:42 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,19 @@
+require 'pry'
+
 class Pirate
+  attr_reader :name, :height, :weight
+
+  PIRATES = []
+
+  def initialize(name, height, weight)
+    @name = name
+    @height = height
+    @weight = weight
+    PIRATES << self
+  end
+
+  def self.all
+    PIRATES
+  end
+
 end
