922123446851e7fd275ab69f7565d38f070267ef
SMartinSci <sm674@cornell.edu>
2018-11-02 18:14:50 -0700

    Done.

--- app.rb
+++ app.rb
@@ -3,7 +3,28 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
+    get '/pirates' do
+      @pirates = Pirate.all
+      erb :/pirates/index'
+    end
+
+    get '/pirates/new' do
+      erb :'pirates/new'
+    end
+
+    get '/pirates/:id' do
+      @pirate = Pirate.find(params[:id])
+    
+      erb :'pirates/show'
+  end
+
+  #  post '/pirates' do
+  #   pirate = Pirate.create(name: params[:pirate][:name], height: params[:pirate])
+  #   params[:pirate][:ships].each do |ship_data|
+  #     ship = Ship.new(ship_data)
+  #     ship.pirate = pirate
+  #     ship.save
+  end
 
   end
 end
