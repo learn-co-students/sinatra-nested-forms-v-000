f36d80558a1134d420b122d4c496799d1cfe6084
Emmy White <emmalinerosewhite@gmail.com>
2018-11-08 09:22:59 -0500

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,20 @@
 class Ship
+  attr_accessor :name, :type, :booty
+  @@shipyard = []
+
+  def initialize(params)
+    @name = params[:name]
+    @type = params[:type]
+    @booty = params[:booty]
+
+    @@shipyard << self
+  end
+
+  def self.all
+    @@shipyard
+  end
+
+  def self.clear
+    @@shipyard.clear
+  end
 end
