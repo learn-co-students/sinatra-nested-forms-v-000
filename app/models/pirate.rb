52d6c53ad0bf2b5def5926cbf14005312f57b4bf
Jeremy <jeremy.emata@gmail.com>
2018-11-16 09:24:52 -0500

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,23 @@
 class Pirate
+
+  attr_accessor :name, :weight, :height
+
+@@all = []
+
+  def initialize(args)
+    @name = args[:name]
+    @weight = args[:weight]
+    @height = args[:height]
+  end
+
+
+  def self.all
+    @@all
+  end
+
+
+  def self.clear
+    @@all = []
+  end
+
 end
