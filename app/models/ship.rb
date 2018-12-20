1847f147583f996c27999a0bc8e5a7dc656291fa
Erin Green <erin.y.green@gmail.com>
2018-12-20 21:28:29 +0000

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,22 @@
 class Ship
+  
+    attr_reader :name, :type, :booty_attributes
+  
+  @@all = []
+  
+  def initalize(params)
+    @name = params[:name]
+    @type = params[:type]
+    @booty_attributes = params[:booty_attributes]
+    @@all << self 
+  end
+  
+  def self.all 
+    @@all 
+  end
+  
+  def self.clear 
+    @@all.clear
+  end
+  
 end
