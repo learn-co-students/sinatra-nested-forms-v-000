63644d316d165b0d788c7bcb05bfe69f4ef91644
Joshua Tan <jtsarvida@gmail.com>
2018-12-22 11:18:43 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,18 @@
 class Pirate
+  
+  attr_reader :name, :weight, :height
+  
+  PIRATES = []
+  
+  def initialize(info)
+    @name = info[:name]
+    @weight = info[:weight]
+    @height = info[:height]
+    PIRATES << self  
+  end
+  
+  def self.all
+    PIRATES
+  end
+  
 end
