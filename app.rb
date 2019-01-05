cc6d11932bb4a23570dbda5ec8804dae476b97e4
Rafael Lopez <lpz.raf@gmail.com>
2019-01-05 01:29:36 +0000

    Done.

--- app.rb
+++ app.rb
@@ -4,6 +4,15 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+  
+  get "/" do 
+    #erb :index
+  end
+  
+  get "/new" do 
+    #erb :index
+  end
+  
 
   end
 end
