2bc0f506e669ae14879d061df654d823b59214ac
yuanxizhang <yuanxi.zhang@gmail.com>
2019-01-27 20:57:27 -0800

    Done

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,16 @@
 class Pirate
+	attr_reader :name, :weight, :height
+
+	@@all_pirate = []
+ 
+  def initialize(params)
+    @name = params[:name]
+    @weight = params[:weight]
+    @height = params[:height]
+    @@all_pirates << self
+  end
+ 
+  def self.all
+    @@all_pirates
+  end
 end
