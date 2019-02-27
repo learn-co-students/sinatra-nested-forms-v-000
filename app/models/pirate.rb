a84e6730f7109c13a8f7488ade772baa0f41f9df
Jonathan Dust <d24dust@hotmail.com>
2019-02-27 15:09:24 -0500

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,25 @@
 class Pirate
+    @@all = []
+    attr_accessor :name, :weight, :height
+
+    def initialize(attributes)
+        attributes.each do |key, value|
+            begin
+               self.send("#{key}=", value) 
+            rescue => exception
+                puts "Did not find #{key}= method"
+            end
+        end
+
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear
+        self.all.clear
+    end
+
 end
