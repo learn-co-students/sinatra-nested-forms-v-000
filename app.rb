68b0dadce970052ae7e4a29234deec8446555349
Ashley Wong <ashwong04@gmail.com>
2019-03-01 20:35:31 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require './environment'
+require 'pry'
 
 module FormsLab
   class App < Sinatra::Base
@@ -11,6 +12,23 @@ module FormsLab
     get '/new' do
       erb :'pirates/new'
     end
+    
+    post '/pirates' do
+      @pirate= params[:pirate]
+      @ship1= params[:ships][0]
+      @ship2= params[:ships][1]
+      erb :'pirates/show'
+    end
 
   end
 end
+
+
+
+# {"pirate"=>
+#   {"name"=>"Ian",
+#   "weight"=>"165",
+#   "height"=>"95",
+#   "ships"=>
+#     [{"name"=>"Titanic", "type"=>"Steam Liner", "booty"=>"Silver"},
+#     {"name"=>"Carpathia", "type"=>"Rescue Ship", "booty"=>"Inner Tubes"}]}}
