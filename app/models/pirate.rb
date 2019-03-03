893a4c50681efe8ee753fb7e1c7e05dedd95dc77
guysbryant <guy.s.bryant@gmail.com>
2019-03-03 11:42:31 -0500

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,15 @@
 class Pirate
+    attr_accessor :name, :weight, :height
+    @@pirates = []
+
+    def initialize(hash)
+        @name = hash[:name]
+        @weight = hash[:weight]
+        @height = hash[:height]
+        @@pirates << self
+    end
+
+    def self.all
+        @@pirates
+    end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,19 @@
 class Ship
+    attr_accessor :name, :type, :booty
+    @@ships = []
+
+    def initialize(hash)
+        @name = hash[:name]
+        @type = hash[:type]
+        @booty = hash[:booty]
+        @@ships << self
+    end
+
+    def self.all
+        @@ships
+    end
+
+    def self.clear
+        @@ships.clear
+    end
 end
