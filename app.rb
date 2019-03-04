7f02ce1a1246b0643dab971a1f34a9c1c58152e7
guysbryant <guy.s.bryant@gmail.com>
2019-03-03 21:16:38 -0500

    Not sure

--- app.rb
+++ app.rb
@@ -8,9 +8,17 @@ module FormsLab
       erb :'pirates/index'
     end
 
+    get '/pirates/new' do
+      erb :'pirates/new'
+    end
+
     get '/pirates/:id' do 
       @pirate = Pirate.find(params[:id])
       erb :'pirates/show'
+    end
+
+    post '/pirates' do
+      binding.pry
     end
 
     # code other routes/actions here
