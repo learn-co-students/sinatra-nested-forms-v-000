908e21a9cce75abf14ee8726fdb2134bac3307be
Heidi Hlavinka <hchlav@gmail.com>
2019-03-15 02:02:34 +0000

    Done.

--- app.rb
+++ app.rb
@@ -10,10 +10,16 @@ module FormsLab
       erb :'pirates/new'
     end
 
-    get '/pirates' do
-      
+    post '/pirates' do
+      @pirate = Pirate.new(params[:pirate])
+
+        params[:pirate][:ships].each do |details|
+          Ship.new(details)
+        end
+
+        @ships = Ship.all
+
+      erb :'pirates/show'
     end
-
-
   end
 end
