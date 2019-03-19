c1ed3c5cd8f8298602e98312f696cf5977aca67a
Cheryl K <cherylkelson@icloud.com>
2019-03-19 00:31:04 +0000

    Done.

--- app.rb
+++ app.rb
@@ -4,22 +4,25 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get '/pirates/new' do
+      erb :'pirates/new'
+    end
+
+
     get '/pirates/:id' do
       @pirates = Pirate.find(params[:id])
       erb :'pirates'
     end
 
 
-    get '/pirates/new' do
-      erb :'pirates/new'
-    end
 
     post '/pirates' do
       @pirates = Pirates.new(params[pirates])
       params[:pirates][ships].each do |details|
-        Ships.new(details)
+      Ships.new(details)
       erb :ship
-    end
+      end
 
+    end
 
 end
