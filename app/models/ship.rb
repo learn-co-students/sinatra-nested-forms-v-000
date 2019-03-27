4174bda9d3d59a2d1fec8a45e4ef0050b2ff7b7c
Angelica Sanchez <anlsanchez@ucdavis.edu>
2019-03-27 22:15:20 +0000

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,21 @@
 class Ship
+  attr_accessor :name, :booty
+  attr_reader :type
+
+  @@all = []
+
+  def initialize(arg)
+    @name = arg[:name]
+    @type = arg[:type]
+    @booty = arg[:booty]
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.clear
+    @@all = []
+  end
 end
