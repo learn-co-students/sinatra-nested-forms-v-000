7700caf84b5b36e006160ee374ff05d9e78362eb
Sheena Bonilla <sheena.myb@gmail.com>
2019-04-03 02:18:25 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,18 @@
+require 'pry'
 class Pirate
+  attr_reader :name, :weight, :height
+  
+  @@all_pirates = []
+  
+  def initialize(hash)
+    hash.each {|k,v| self.send("#{k}=", v)}
+  end
+  
+  def self.all 
+    @@all_pirates
+  end
+  
+  def self.clear
+    @@all_pirates.clear
+  end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,13 @@
 class Ship
+  attr_reader :name, :type, :booty
+  
+  @@all_ships = []
+  
+  def initialize(hash)
+    hash.each {|k,v| self.send("#{k}=", v)}
+  end
+  
+  def self.all
+    @@all_ships
+  end
 end
