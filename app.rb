42432423b72588a3dddf1b36de907a82a0797a2f
Mariel Hoepelman <marielhoepelman@gmail.com>
2019-04-15 20:02:02 +0000

    WIP

--- app.rb
+++ app.rb
@@ -8,7 +8,11 @@ module FormsLab
     end
 
     get '/new' do
-      erb :new
+     erb :"pirates/new"
+    end
+
+    post '/pirates' do
+      erb :"pirates/show"
     end
   end
 end
--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -13,5 +13,5 @@ class Pirate
 
   def self.all
    PIRATES
- end
+  end
 end
