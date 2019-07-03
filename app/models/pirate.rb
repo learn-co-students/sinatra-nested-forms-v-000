7161b823b57aab88a4ec89a19ff625e11486d26f
Steven Crouse <sdcrouse91@gmail.com>
2019-07-03 00:41:42 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,16 @@
 class Pirate
+  attr_reader :name, :weight, :height
+  @@all_pirates = []
+  
+  def initialize(pirate_params)
+    @name = pirate_params[:name]
+    @weight = pirate_params[:weight]
+    @height = pirate_params[:height]
+    
+    @@all_pirates << self
+  end
+  
+  def self.all 
+    @@all_pirates
+  end
 end
