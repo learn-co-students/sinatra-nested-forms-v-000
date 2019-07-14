5ae414d188ba5e1e3e8929a229d471cd9086b230
Dianne Jarvis-Lumsden <diannejarvisj@gmail.com>
2019-07-14 03:30:26 +0000

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,19 @@
 class Pirate
+
+  attr_accessor :name, :weight, :height
+
+  def initialize(name, weight, height)
+    @name = name
+    @weight = weight
+    @height = height
+  end
+
+  def self.all
+    self
+  end
+
+  def self.clear
+
+  end
+
 end
