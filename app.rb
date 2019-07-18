6322a81a8e4c9c9319862b3208a8577fe5717bad
Sheena Bonilla <sheena.myb@gmail.com>
2019-07-18 04:57:33 +0000

    Done.

--- app.rb
+++ app.rb
@@ -7,10 +7,18 @@ module FormsLab
     get '/' do
      erb :root
     end
-    
+
     get '/new' do
      erb :new
     end
-  
+
+    post '/pirates' do
+     @pirate = Pirate.new(params["pirate"])
+     ships = params["pirate"]["ships"]
+     #binding.pry
+     @ship_1 = Ship.new(ships[0])
+     @ship_2 = Ship.new(ships[1])
+     erb :show
+    end
   end
 end
