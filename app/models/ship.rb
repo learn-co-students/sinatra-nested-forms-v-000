b2fb97a6358f70fa4f10b593595493a7d971c47e
Diapause <diapause@protonmail.com>
2019-07-31 13:37:27 -0700

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,13 @@
 class Ship
+  attr_reader :name, :type, :booty
+
+  def initialize(name, type, booty)
+    @name = name
+    @type = type
+    @booty = booty
+  end
+
+  def self.clear
+    Pirate.all.clear
+  end
 end
