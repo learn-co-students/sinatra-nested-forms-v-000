b81f8283815fc0d46ef883cdf7850bfdc0c78ab1
Chao Hsuan Chi <allen0413@gmail.com>
2019-09-25 03:38:02 +0000

    Done.

--- app.rb
+++ app.rb
@@ -4,6 +4,21 @@ module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get '/' do
+      erb :new
+    end
+
+    post '/show' do
+      @student = Student.new(params[:student])
+
+      params[:student][:courses].each do |details|
+        Course.new(details)
+      end
+
+      @courses = Course.all
+
+      erb :show
+    end
 
   end
 end
--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,15 @@
 class Pirate
+  attr_reader :weight, :height
+
+  PIRATES = []
+
+  def initialize(params)
+    @weight = params[:weight]
+    @height = params[:height]
+    PIRATES << self
+  end
+
+  def self.all
+    PIRATES
+  end
 end
