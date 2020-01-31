af789000a2c719aec82e0eac740070c82946d6ed
Niusha H <niusha.hadziomerovic@gmail.com>
2020-01-31 18:50:09 +0000

    Done.

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -2,15 +2,18 @@ class Ship
   attr_accessor :name, :type, :booty
 
   @@all = []
-
-  def initialize
+ 
+  def initialize(params)
+    @name = params[:name]
+    @weight = params[:type]
+    @height = params[:booty]
     @@all << self
   end
-
+ 
   def self.all
     @@all
   end
-
+  
   def self.clear
     @@all.delete
   end
