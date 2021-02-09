804ecf18818c84d84d30883232cbebc36ff8dcda
andy-aguilar <aaguil3@gmail.com>
2021-02-09 09:38:09 -0600

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,29 @@
 class Pirate
+    attr_accessor :name, :weight, :height
+
+    @@all = []
+
+    def initialize(args)
+        args.each do |key, value|
+            self.send("#{key.to_s}=", value)
+        end
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def ships=(ships)
+        ships.each do |ship|
+            if ship[:name] != ""
+                ship_obj = Ship.new(ship)
+                ship_obj.pirate = self
+            end
+        end
+    end
+
+    def ships
+        Ship.all.select{|ship| ship.pirate == self}
+    end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,20 @@
 class Ship
+    attr_accessor :name, :type, :booty, :pirate
+
+    @@all = []
+
+    def initialize(args)
+        args.each do |key, value|
+            self.send("#{key.to_s}=", value)
+        end
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear
+        @@all = []
+    end
 end
