af2eb33b816782d86009b4a9a2d10d2f016689b3
Daniel Suarez <suarez.daniel101@gmail.com>
2021-06-02 23:46:31 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,9 +1,21 @@
 require './environment'
+require 'pry'
 
 module FormsLab
   class App < Sinatra::Base
 
     # code other routes/actions here
+    get '/' do
+      erb :root
+    end
 
+    get '/new' do
+      erb :"pirates/new"
+    end
+
+    post '/pirates' do
+      binding.pry
+      erb :"pirates/show"
+    end
   end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,19 @@
 class Ship
+    attr_accessor :type, :booty
+    
+    @@all = []
+
+    def initialize(params)
+      @type = params[:type]
+      @booty = params[:booty]
+      @@all << self
+    end
+
+    def self.all
+      @@all
+    end
+
+    def self.clear
+      @@all.clear
+    end
 end
